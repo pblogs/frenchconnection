64b2a8e2233a3d3c3608e5d64f100d7f24ca98ff
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-04-06 09:20:35 +0200

    started on static pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 AllieroForms::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
+  # The priority is based upon order of 
+  # creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static_pages#frontpage_manager'
+  get '/frontpage_manager' => 'static_pages#frontpage_manager', as: :frontpage_manager
+  get '/new_assignment'    => 'static_pages#new_assignment', as: :new_assignment
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
