20e430c1eb0b1e304a3e4d8688572603ce506308
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-04-06 09:20:25 +0200

    added slim, spec and fabrication

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,10 @@ Bundler.require(*Rails.groups)
 
 module AllieroForms
   class Application < Rails::Application
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.initialize_on_precompile = true
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -24,5 +28,19 @@ module AllieroForms
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.generators do |g|
+      g.fixture_replacement :fabrication
+      g.template_engine     :slim
+      g.test_framework      :rspec,
+        views: false,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+
+    end
   end
 end
