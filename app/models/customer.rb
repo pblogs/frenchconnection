3730ea814bc51091dcd886b7347a8acae35ad91a
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-04-06 13:49:36 +0200

    div

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,7 @@
 class Customer < ActiveRecord::Base
+  validates :name,           :presence => true          
+  validates :address,        :presence => true
+  validates :org_number,     :presence => true
+  validates :contact_person, :presence => true
+  validates :phone,          :presence => true
 end
