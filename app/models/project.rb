263a52bd73b93ed5ea9ab56f45c69ff496626762
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-04-15 12:56:43 +0200

    a project belongs to a customer

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,7 @@
 class Project < ActiveRecord::Base
-  belongs_to :company
+  belongs_to :customer
   has_many :tasks
+
+  validates :customer_id, :presence => true
+  validates :name,        :presence => true
 end
