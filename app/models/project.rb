720a1b2165aac2d40e8c99cc0e22ced1468f472f
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-04-15 16:25:37 +0200

    project has many hours spents trough tasks.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ActiveRecord::Base
   belongs_to :customer
   has_many :tasks
+  has_many :hours_spents, :through => :tasks
 
   validates :customer_id, :presence => true
   validates :name,        :presence => true
