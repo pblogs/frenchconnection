c725809bb4e881d1bb1570ef66b56be64314db39
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-04-15 11:47:44 +0200

    a task belongs to project

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140414083801) do
+ActiveRecord::Schema.define(version: 20140415094301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,16 @@ ActiveRecord::Schema.define(version: 20140414083801) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "projects", force: true do |t|
+    t.string   "project_number"
+    t.string   "name"
+    t.integer  "company_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "projects", ["company_id"], name: "index_projects_on_company_id", using: :btree
 
   create_table "task_types", force: true do |t|
     t.string   "title"
