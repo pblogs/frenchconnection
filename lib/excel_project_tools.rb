4c1510c3b0f2f796622a8e3f25764f410f1b67ab
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-04-16 15:59:07 +0200

    outputting the artisan names correct.

--- lib/excel_project_tools.rb
+++ lib/excel_project_tools.rb
@@ -2,12 +2,15 @@ class ExcelProjectTools
 
   def self.hours_for_artisans(project)
     hours = []
-    puts "\n\n\n project.artisans: #{project.class} \n\n\n"
     project.artisans.each do |a|
       hours << project.hours_total_for(a)
     end
-    #hours
     hours.map {|x| "#{x}" }
+  end
+
+  def self.artisan_names(project)
+    project.name_of_artisans.split(',')
+    # TODO Remove sourrunding whitespace
   end
 
 end
