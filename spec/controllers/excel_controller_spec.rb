c99aefc5eced8944cf0531ac9c31da35f91c7885
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-04-16 14:02:00 +0200

    put test as pending.

--- spec/controllers/excel_controller_spec.rb
+++ spec/controllers/excel_controller_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe ExcelController do
 
   describe "GET 'export'" do
+    pending
     it "returns http success" do
       get 'export'
       response.should be_success
