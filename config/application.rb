e75e185f35c112e2bdb17a044da6d27104dbaa81
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-04-22 13:00:50 +0200

    added action_mailer.smtp_settings

--- config/application.rb
+++ config/application.rb
@@ -42,7 +42,19 @@ module AllieroForms
         routing_specs: false,
         controller_specs: true,
         request_specs: false
-
     end
+
+
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               ENV['SMTP_DOMAIN'],
+      user_name:            ENV['SMTP_USERNAME'],
+      password:             ENV['SMTP_PASSWORD'],
+      authentication:       'plain',
+      enable_starttls_auto: true  
+    }
+
   end
 end
