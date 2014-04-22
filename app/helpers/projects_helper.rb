6d9ecebd013d8be15267f6d45bdfb130509f9c1a
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-04-22 07:58:42 +0200

    a project can be paid_pr_the_hour or fixed.

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,13 @@
 module ProjectsHelper
+
+  def payment(project)
+    if project.paid_by_the_hour
+      raise 'her'
+      content_tag(:p) do
+        content_tag(:span) do
+          "Pris pr time: #{project.price_pr_hour}"
+        end
+      end
+    end
+  end
 end
