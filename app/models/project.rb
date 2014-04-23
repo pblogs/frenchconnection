15aafabc9607815231064082b2719a8fb6ad19d2
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-04-23 19:06:38 +0200

    removed project name from form

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,7 +5,6 @@ class Project < ActiveRecord::Base
   has_many :artisans,     :through => :tasks
 
   validates :customer_id, :presence => true
-  validates :name,        :presence => true
   validates :start_date,  :presence => true
   validates :due_date,    :presence => true
 
