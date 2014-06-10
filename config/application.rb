d866d89068b73da8d8e8a7218c98bc02d37e67ce
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-06-10 20:38:59 +0200

    Locale

--- config/application.rb
+++ config/application.rb
@@ -32,8 +32,9 @@ module AllieroForms
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    #config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = "nb-no"
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.default_locale = "nb"
 
     config.generators do |g|
       g.fixture_replacement :fabrication
