3f4f8cc6f15ad3dad0212d05161a34da4011b6fa
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-06-10 20:38:59 +0200

    Locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   
 
-  before_action :set_current_user, if: :signed_in?
+  before_action :set_current_user
   before_action :authenticate_user!
 
   def set_current_user
