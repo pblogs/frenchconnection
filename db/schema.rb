291d745184a3857e0c5b484ed365847e34908c97
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-06-12 13:51:05 +0200

    switched to mobile as login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140606093100) do
+ActiveRecord::Schema.define(version: 20140612114429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -123,13 +123,13 @@ ActiveRecord::Schema.define(version: 20140606093100) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "",       null: false
-    t.string   "encrypted_password",     default: "",       null: false
-    t.string   "roles",                  default: "worker", null: false
+    t.string   "email",                  default: "",         null: false
+    t.string   "encrypted_password",     default: "",         null: false
+    t.string   "roles",                  default: ["worker"],              array: true
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,        null: false
+    t.integer  "sign_in_count",          default: 0,          null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -139,6 +139,7 @@ ActiveRecord::Schema.define(version: 20140606093100) do
     t.string   "first_name"
     t.string   "last_name"
     t.integer  "department_id"
+    t.integer  "mobile"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
