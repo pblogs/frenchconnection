e858887ff6af26b56e2e08dc7b3dcb1a2de7ebaa
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-06-12 21:13:14 +0200

    update test

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -2,7 +2,7 @@ Fabricator(:user) do
   first_name { Faker::Name.first_name }
   last_name  { Faker::Name.last_name }
   email      { Faker::Internet.email }
-  mobile     { Faker::PhoneNumber.cell_phone }
+  mobile     { Fabricate.sequence(:number, 93441707) }
   password { 'topsecret' }
   password_confirmation { 'topsecret' }
 end
