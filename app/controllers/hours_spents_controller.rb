de83d060d9bc8b0bfef2b6268ae3624f8a5a2957
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-06-12 20:57:40 +0200

    fix locale

--- app/controllers/hours_spents_controller.rb
+++ app/controllers/hours_spents_controller.rb
@@ -29,7 +29,7 @@ class HoursSpentsController < ApplicationController
     respond_to do |format|
       if @hours_spent.save
         format.html { redirect_to @hours_spent, 
-                      notice: 'Hours spent was successfully created.' }
+                      notice: 'Timer registrert' }
         format.json { render action: 'show', 
                       status: :created, location: @hours_spent }
       else
@@ -46,7 +46,7 @@ class HoursSpentsController < ApplicationController
     respond_to do |format|
       if @hours_spent.update(hours_spent_params)
         format.html { redirect_to @hours_spent, 
-                      notice: 'Hours spent was successfully updated.' }
+                      notice: 'Antall timer oppdatert' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
