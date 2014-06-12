0e76b1c786c975bb9659bae1b0980f7c69112298
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-06-12 21:00:37 +0200

    remove task_type

--- spec/fabricators/task_fabricator.rb
+++ spec/fabricators/task_fabricator.rb
@@ -1,6 +1,5 @@
 Fabricator(:task) do
   project       { Fabricate(:project) }
-  task_type     { Fabricate(:task_type) }
   start_date    "2014-04-06 10:55:29"
   due_date       { Time.now.next_week }
   accepted true
