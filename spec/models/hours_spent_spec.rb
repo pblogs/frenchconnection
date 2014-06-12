383ad3e20fada09727a7bbd60bc2fd0329870ab8
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-06-12 21:00:46 +0200

    new test

--- spec/models/hours_spent_spec.rb
+++ spec/models/hours_spent_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe HoursSpent do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) do
+    @worker  = Fabricate(:user, roles: 'worker', first_name: 'John')
+    @task    = Fabricate(:task)
+    @task.user_ids = [@worker.id]
+    @task.save
+    Fabricate(:hours_spent, user: @worker, task: @task, hour: 10)
+    Fabricate(:hours_spent, user: @worker, task: @task, piecework_hours: 10)
+    Fabricate(:hours_spent, user: @worker, task: @task, piecework_hours: 30)
+    Fabricate(:hours_spent, user: @worker, task: @task, piecework_hours: 50)
+  end
+
+  it "knows how many hour a worker har registred" do
+    @task.hours_total.should eq 100
+  end
 end
