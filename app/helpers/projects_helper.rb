623a201b1af89d7d8d407867d4073c3c1caf823e
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-06-13 17:00:18 +0200

    better fallback for missing project number.

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -17,7 +17,7 @@ module ProjectsHelper
   end
 
   def project_number(p)
-    p.project_number.present? ? p.project_number : 'Mangler prosjektnummer'
+    p.project_number.present? ? p.project_number : "Prosjekt ##{p.id}"
   end
 
 end
