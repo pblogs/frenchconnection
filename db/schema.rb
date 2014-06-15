66d4cdd6464ee69d26e1acdf452633744c66f253
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-06-15 15:13:37 +0200

    schama.rb - update default roles.

--- db/schema.rb
+++ db/schema.rb
@@ -123,13 +123,13 @@ ActiveRecord::Schema.define(version: 20140612114429) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "",       null: false
-    t.string   "encrypted_password",     default: "",       null: false
-    t.string   "roles",                  default: "worker", null: false
+    t.string   "email",                  default: "",         null: false
+    t.string   "encrypted_password",     default: "",         null: false
+    t.string   "roles",                  default: ["worker"],              array: true
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,        null: false
+    t.integer  "sign_in_count",          default: 0,          null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
