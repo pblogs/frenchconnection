021754808bc5cd1bfcc2962b319f87faaa3d4277
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-06-15 15:49:18 +0200

    fix wrong name of users and avatars.

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -2,7 +2,7 @@ Fabricator(:user) do
   first_name { Faker::Name.first_name }
   last_name  { Faker::Name.last_name }
   email      { Faker::Internet.email }
-  mobile     { Fabricate.sequence(:number, 93441707) }
+  mobile     { Fabricate.sequence(:number, 93000000) }
   password { 'topsecret' }
   password_confirmation { 'topsecret' }
 end
