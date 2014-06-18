f350c43ebc12ed472149151efd0041949f68b331
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-06-18 16:10:52 +0200

    schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140612114429) do
+ActiveRecord::Schema.define(version: 20140618131626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "api_keys", force: true do |t|
+    t.string  "name"
+    t.string  "access_token"
+    t.boolean "active"
+  end
 
   create_table "artisans", force: true do |t|
     t.string   "name"
