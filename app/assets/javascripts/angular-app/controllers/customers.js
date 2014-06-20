3004a0b7f50582f713d0c47449a32b49b3b3aa34
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-06-20 15:26:52 +0200

    data binding working at customer/form

--- app/assets/javascripts/angular-app/controllers/customers.js
+++ app/assets/javascripts/angular-app/controllers/customers.js
@@ -2,18 +2,27 @@ angular
   .module('app')
   .controller('CustomersCtrl', ['Customer', '$scope', 
     function(Customer, $scope) {
-      $scope.customers = Customer.query();
+      this.customer = {};
 
-      $scope.save = function(customer) {
-        if (!_.include($scope.todos, customer)) {
-          $scope.todos.push(customer);
-          customer.$save();
-        } else {
-          Todo.update(customer);
-        }
-        $scope.activeCustomer = new Customer();
-      };
 
+      //$scope.customers = Customer.query();
+
+      //$scope.save = function(customer) {
+      //  if (!_.include($scope.todos, customer)) {
+      //    $scope.todos.push(customer);
+      //    customer.$save();
+      //  } else {
+      //    Customer.update(customer);
+      //  }
+      //  $scope.activeCustomer = new Customer();
+      //};
+
+
+      //$scope.activeCustomer = new Customer();
+
+      //$scope.edit = function(todo) {
+      //  $scope.activeCustomer = todo;
+      //};
 
 
   }]);
