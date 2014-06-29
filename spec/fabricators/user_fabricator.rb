3771f79fbd691c5efb269a641f0f96e1ae11a368
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-06-29 10:25:52 +0200

    update User fabricator with Faker number

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -2,7 +2,7 @@ Fabricator(:user) do
   first_name { Faker::Name.first_name }
   last_name  { Faker::Name.last_name }
   email      { Faker::Internet.email }
-  mobile     { Faker::PhoneNumber.phone_number }
+  mobile     { Faker::Base.numerify('########') }
   password { 'topsecret' }
   password_confirmation { 'topsecret' }
 end
