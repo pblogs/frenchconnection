008e5cd9d689537166c2c0b0d98fa0dda4e31e2b
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-06-30 09:43:47 +0200

    Don't spesify any domains in the URL

--- app/assets/javascripts/angular-app/models/customer.js
+++ app/assets/javascripts/angular-app/models/customer.js
@@ -2,7 +2,7 @@ angular
   .module('app')
   .factory('Customer', function($resource) {
 
-    var Customer = $resource('http://allieroforms.dev/api/v1/customers/:id.json', {id: '@id'}, {
+    var Customer = $resource('/api/v1/customers/:id.json', {id: '@id'}, {
       update: {
         method: 'PUT'
       }
