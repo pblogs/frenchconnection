3f4ee71cc2f1c8294fc1bd98b8e7a7c7d46d2a5b
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-07-03 21:17:14 +0200

    Switched ngResource with Restangular

--- app/assets/javascripts/angular-app/app.js
+++ app/assets/javascripts/angular-app/app.js
@@ -1,5 +1,5 @@
 angular
-  .module('app', ['ngRoute', 'ngResource'])
+  .module('app', ['ngRoute', 'restangular'])
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/', {
       controller: 'MainCtrl'
