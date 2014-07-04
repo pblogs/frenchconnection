4f03c8cf33195b3b8dc34a82d14807b6713ea60d
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-07-04 11:18:17 +0200

    comment out angular routes. not using them now.

--- app/assets/javascripts/angular-app/app.js
+++ app/assets/javascripts/angular-app/app.js
@@ -1,21 +1,12 @@
 angular
   .module('app', ['ngRoute', 'restangular'])
-
-   //.config(function (RestangularProvider, baseRequestConfig, $routeProvider, urlsProvider) {
-  //    })
-
   .config(['$routeProvider', 'RestangularProvider', function($routeProvider, RestangularProvider) {
     RestangularProvider.setBaseUrl('/api/v1');
-    $routeProvider.when('/', {
-      controller: 'MainCtrl'
-    }).when('/:status', {
-      controller: 'HomeCtrl',
-    }).otherwise({
-      redirectTo: '/'
-    });
+    // $routeProvider.when('/', {
+    //   controller: 'MainCtrl'
+    // }).when('/:status', {
+    //   controller: 'HomeCtrl',
+    // }).otherwise({
+    //   redirectTo: '/'
+    // });
   }]);
-
-  // .config(function(RestangularProvider) {
-  // });
-
-
