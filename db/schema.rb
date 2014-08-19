e6eced5997db3632251b9b28e2cdc385e684ff9e
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-08-19 14:23:33 +0200

    email not mandatory for user

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140618131626) do
+ActiveRecord::Schema.define(version: 20140819121020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "api_keys", force: true do |t|
-    t.string  "name"
-    t.string  "access_token"
-    t.boolean "active"
-  end
 
   create_table "artisans", force: true do |t|
     t.string   "name"
@@ -129,13 +123,13 @@ ActiveRecord::Schema.define(version: 20140618131626) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "",       null: false
-    t.string   "encrypted_password",     default: "",       null: false
-    t.string   "roles",                  default: "worker", null: false
+    t.string   "email",                  default: ""
+    t.string   "encrypted_password",     default: "",         null: false
+    t.string   "roles",                  default: ["worker"],              array: true
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,        null: false
+    t.integer  "sign_in_count",          default: 0,          null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
