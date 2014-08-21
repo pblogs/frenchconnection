b2c0f425d1fe30ff2016d9a077145a3e397813cf
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-08-21 13:11:17 +0200

    Import customer task done. :)
    $ thor customers:import ExcelSheetsFromAlliero/Kopi\ av\ A<030a>pne\ kunder\ Visma\ Global\ -\ Alliero\ AS.xlsx

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140820112251) do
+ActiveRecord::Schema.define(version: 20140821105230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 20140820112251) do
     t.string   "phone"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "customer_nr"
+    t.string   "area"
   end
 
   create_table "departments", force: true do |t|
