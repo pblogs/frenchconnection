eaa988cc1549ab4fb8b4af741fd1e44b8d6fedf0
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-08-21 13:25:18 +0200

    email added to customers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140821105230) do
+ActiveRecord::Schema.define(version: 20140821111321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20140821105230) do
     t.datetime "updated_at"
     t.integer  "customer_nr"
     t.string   "area"
+    t.string   "email"
   end
 
   create_table "departments", force: true do |t|
