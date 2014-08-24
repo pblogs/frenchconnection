ce8770493b32cb4d2588856086860887297a1c26
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-08-24 16:01:20 +0200

    dont initialize Clickatell in test env

--- config/initializers/clickatell.rb
+++ config/initializers/clickatell.rb
@@ -1,1 +1,3 @@
-SMS = Clickatell::API.authenticate( ENV['CLICKATELL_API_ID'], ENV['CLICKATELL_USERNAME'], ENV['CLICKATELL_PASSWORD'] )
+unless Rails.env.test?
+  SMS = Clickatell::API.authenticate( ENV['CLICKATELL_API_ID'], ENV['CLICKATELL_USERNAME'], ENV['CLICKATELL_PASSWORD'] )
+end
