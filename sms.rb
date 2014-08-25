721569301195244f76cf917f7027fc27ec360feb
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-08-25 08:04:23 +0200

    remove sms.rb

--- sms.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems'
-require 'clickatell'
-
-api = Clickatell::API.authenticate('3494167', 'orwapp_alliero', 'PPIKCMYONgdcZS')
-api.send_message('4793441707', 'Hello from ruby scrupt')
-
-
-#http://api.clickatell.com/http/sendmsg?user=orwapp_alliero&password=PPIKCMYONgdcZS&api_id=3494167&to=4793441707&text=Message
