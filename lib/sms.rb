96c6e286b1269e3104457520f26a205e1f4b8889
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-08-25 08:23:17 +0200

    sms.rb is a lib not an initialiser.

--- lib/sms.rb
+++ lib/sms.rb
@@ -1,9 +1,7 @@
-require 'iconv'
-
 class Sms
   def self.send_msg(to, msg)
     url = "http://bulksms.vsms.net:5567/eapi/submission/send_sms/2/2.0?" + 
-      "username=#{ENV['BULKSMS_USERNAME']}&password=#{ENV['BULKSMS_USERNAME']}&sender=Orwapp"+
+      "username=#{ENV['BULKSMS_USERNAME']}&password=#{ENV['BULKSMS_PASSWORD']}&sender=Orwapp"+
       "&allow_concat_text_sms=1&concat_text_sms_max_parts=8&want_report=8&message=#{encode(msg)}&msisdn=#{to}"
     resp = Net::HTTP.get(URI.parse(url))
     puts resp
