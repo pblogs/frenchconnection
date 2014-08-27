5f1d10f9a2de5807e75ba3dd814b56c0856fd8f3
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-08-27 09:42:03 +0200

    Updated the users table to not fail on CodeClimate

--- db/schema.rb
+++ db/schema.rb
@@ -140,12 +140,12 @@ ActiveRecord::Schema.define(version: 20140826124448) do
 
   create_table "users", force: true do |t|
     t.string   "email"
-    t.string   "encrypted_password",         null: false
-    t.string   "roles",                      array: true
+    t.string   "encrypted_password",                           null: false
+    t.string   "roles",                                                     array: true
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",              null: false
+    t.integer  "sign_in_count",                    default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
