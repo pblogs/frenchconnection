f700b7f3f76e9960e6754828cf970493d044e63b
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-08-29 11:39:33 +0200

    created Profession model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140828125916) do
+ActiveRecord::Schema.define(version: 20140829081753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,12 +37,6 @@ ActiveRecord::Schema.define(version: 20140828125916) do
     t.datetime "updated_at"
   end
 
-  create_table "categories", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "customer_messages", force: true do |t|
     t.string   "text"
     t.datetime "created_at"
@@ -64,9 +58,7 @@ ActiveRecord::Schema.define(version: 20140828125916) do
   end
 
   create_table "departments", force: true do |t|
-    t.string   "title"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "title"
   end
 
   create_table "hours_spents", force: true do |t|
