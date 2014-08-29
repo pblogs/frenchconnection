7ec810ec4c6e91e9f4715b1e2f7dc0c6505fad35
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-08-29 09:31:46 +0200

    use keyword arguments

--- spec/lib/dagsrapport_spec.rb
+++ spec/lib/dagsrapport_spec.rb
@@ -6,6 +6,8 @@ describe Dagsrapport do
     @project = Fabricate(:project)
     @user    = Fabricate(:user, first_name: 'John')
     @task    = Fabricate(:task, project: @project)
+    @profession = Fabricate(:profession, title: 'Snekker')
+    @overtime = 50
 
     Fabricate(:hours_spent, hour: 10,        
                task: @task, user: @user )
@@ -14,7 +16,7 @@ describe Dagsrapport do
   end
 
   it 'has no syntax errors' do
-    Dagsrapport.new(@project)
+    Dagsrapport.new(project: @project, profession: @profession, overtime: 100)
       .create_spreadsheet.class.should eq String
   end
 
