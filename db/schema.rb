984c969082eee200eb49cd4b8500e27b8034a7a3
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-08-29 14:38:53 +0200

    Let all users belong to 545 Bratfoss department

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140829115255) do
+ActiveRecord::Schema.define(version: 20140829123427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
