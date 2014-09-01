4c9d3471675ec86061c79a06017a12d16acb42ed
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-09-01 08:37:35 +0200

    updated fabricator

--- spec/fabricators/customer_fabricator.rb
+++ spec/fabricators/customer_fabricator.rb
@@ -1,8 +1,8 @@
 Fabricator(:customer) do
   name           { Faker::Company.name }
-  address        { Faker::Address.street_name } 
-  org_number    { Random.new_seed.to_s[0..6] }
+  address        { Faker::Address.street_address } 
+  org_number     { Random.new_seed.to_s[0..6] }
   customer_nr    { Random.new_seed.to_s[0..6] }
   contact_person { Faker::Name.first_name }
-  phone     { Faker::Base.numerify('########') }
+  phone          { Faker::Base.numerify('########') }
 end
