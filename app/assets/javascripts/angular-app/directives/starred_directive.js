8f9fb2e51bea8020fddb81c3612f5ca583e2abaa
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-09-03 09:28:29 +0200

    all attributes are set directly on starred

--- app/assets/javascripts/angular-app/directives/starred_directive.js
+++ app/assets/javascripts/angular-app/directives/starred_directive.js
@@ -3,14 +3,15 @@ angular.module('app', [])
   return {
     restrict: 'E',
     scope: {
-      objectClass: '='
+      objectClass: '=',
+      objectId: '='
     },
     templateUrl: '/templates/starred.html.slim',
     controller: function($scope, $attrs) {
       return $scope.star_clicked = function(e) {
         var id = $(e.target).closest('tr').data('customer-id');
-        console.log("info: " + $attrs.objectClass);
-        console.log("id: " + id);
+        //console.log("info: " + $attrs.objectClass);
+        //console.log("info: " + $attrs.objectId);
       };
     }
   };
