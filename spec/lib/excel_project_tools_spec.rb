8cd68239f58b8442124a4b804e348853a7a6d074
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-09-03 14:34:36 +0200

    added tests

--- spec/lib/excel_project_tools_spec.rb
+++ spec/lib/excel_project_tools_spec.rb
@@ -16,15 +16,44 @@ describe ExcelProjectTools do
   end
 
   it %q{returns a comma separated 
-  string with the sum of the hours each user has worked} do
+        string with the sum of the hours each user has worked} do
     snekker = Profession.where(title: 'Snekker')
-    ExcelProjectTools.hours_for_users(project: @project, profession: snekker).should eq ['19']
+    ExcelProjectTools.hours_for_users(project: @project, 
+      profession: snekker).should eq ['19']
   end
 
 
   it %q{returns a comma separated string of names for profession} do
     ExcelProjectTools.user_names(project: @project, 
-                                 profession_title: 'Snekker').should eq ['Snekker Jens']
+      profession_title: 'Snekker').should eq ['Snekker Jens']
+  end
+
+  it 'sum_piecework_hours' do
+    Fabricate(:hours_spent, piecework_hours: 10, 
+              task: @task, user: @user1, project: @project )
+    ExcelProjectTools.sum_piecework_hours(project: @project, 
+                                          user: @user1 ).should eq 10
+  end
+
+  it 'sum_workhours' do
+    Fabricate(:hours_spent, piecework_hours: 16, 
+              task: @task, user: @user1, project: @project )
+    ExcelProjectTools.sum_piecework_hours(project: @project, 
+                                          user: @user1 ).should eq 16
+  end
+
+  it 'sum_overtime_50' do
+    Fabricate(:hours_spent, piecework_hours: 50, 
+              task: @task, user: @user1, project: @project )
+    ExcelProjectTools.sum_piecework_hours(project: @project, 
+                                          user: @user1 ).should eq 50
+  end
+
+  it 'sum_overtime_100' do
+    Fabricate(:hours_spent, piecework_hours: 100, 
+              task: @task, user: @user1, project: @project )
+    ExcelProjectTools.sum_piecework_hours(project: @project, 
+                                          user: @user1 ).should eq 100
   end
 
 end
