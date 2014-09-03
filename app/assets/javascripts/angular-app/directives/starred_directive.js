1d37226d42cdcc941b17b4a34ceeacf80816d8bf
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-09-03 09:23:59 +0200

    The starred directive know which id and class

---
+++ app/assets/javascripts/angular-app/directives/starred_directive.js
@@ -0,0 +1,17 @@
+angular.module('app', [])
+.directive('starred', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      objectClass: '='
+    },
+    templateUrl: '/templates/starred.html.slim',
+    controller: function($scope, $attrs) {
+      return $scope.star_clicked = function(e) {
+        var id = $(e.target).closest('tr').data('customer-id');
+        console.log("info: " + $attrs.objectClass);
+        console.log("id: " + id);
+      };
+    }
+  };
+});
