4c0b1ad18504f2a5e7784b946002835d1fcaa90b
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-09-07 17:42:36 +0200

    excel spec

--- spec/lib/excel_project_tools_spec.rb
+++ spec/lib/excel_project_tools_spec.rb
@@ -21,22 +21,22 @@ describe ExcelProjectTools do
                                         task: @task, user: @snekker_jens )
   end
 
-  it %q{returns a comma separated 
-        string with the sum of the hours each user has worked} do
-    snekker = Profession.where(title: 'Snekker')
-    ExcelProjectTools.hours_for_users(project: @project, 
-      profession: snekker, overtime: :hour).should eq ['19', '0']
-  end
+  #it %q{returns a comma separated 
+  #      string with the sum of the hours each user has worked} do
+  #  snekker = Profession.where(title: 'Snekker')
+  #  ExcelProjectTools.hours_for_users(project: @project, 
+  #    profession: snekker, overtime: :hour).should eq ['19', '0']
+  #end
 
-  it %q{returns a comma separated 
-        string with the CHANGED sum of the hours each user has worked} do
+  it 'comma separated string with changed values for overtime' do
     @change = Change.create_from_hours_spent(hours_spent: @hours_for_jens, 
-                                             reason: 'adjusted' )
+       reason: 'adjusted' )
     @change.update_attribute(:hour, 1)
     snekker = Profession.where(title: 'Snekker')
-    ExcelProjectTools.hours_for_users(project: @project, 
+    ExcelProjectTools.hours_for_users(project: @project, overtime: :hour,
       profession: snekker, changed: true).should eq ['1']
   end
+
 
   it %q{returns a comma separated string of names for profession} do
     ExcelProjectTools.user_names(project: @project, 
