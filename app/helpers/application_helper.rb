3c277c44221e940b5a186d4f5336dc61b1969755
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-09-09 16:04:30 +0200

    offset helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def body_id
     "#{controller.controller_name}_#{controller.action_name}"
   end
+
+  def return_users_nr_in_array(array, user: user)
+    array.find_index(user)
+  end
 end
