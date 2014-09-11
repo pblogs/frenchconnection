1b22a5f5ec8f99535414f71c2d8a504229b03b8b
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-09-11 14:43:33 +0200

    add version to the API

--- app/api/v1/base.rb
+++ app/api/v1/base.rb
@@ -1,7 +1,7 @@
 #require 'v1/entities/users'
 module V1
   class Base < Grape::API
-    #version 'v1', using: :path, vendor: 'orwapp', cascade: false
+    version 'v1', using: :path, vendor: 'orwapp', cascade: false
 
     mount Users
     #mount V1::Entities::Users
