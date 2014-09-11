9d05cd889c3d42b6e0cab5fb1b9e99687b941a77
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-09-11 14:40:26 +0200

    renamed ads to users

--- app/api/v1/entities/users.rb
+++ app/api/v1/entities/users.rb
@@ -1,30 +1,19 @@
 module V1
   module Entities
     class Users < Grape::Entity
-      expose :id, :created_at, :updated_at, :email
-      expose :first_name, :last_name, :profile_url, :image_url
-      #expose :profile_image, as: :image_info, using: V1::Entities::Attachment,
-      #  if: lambda { |user, options|
-      #    user.profile_image.class.name == 'Attachment' }
-      #expose :ads, using: V1::Entities::Ads, if: { include: :user_ads }
+      expose :id, :state, :state_events, :url
+      #expose :created_at, :updated_at, :sold_at
+      #expose :description, :title, :price
+      #expose :address, :lat, :lon
+      #expose :user,  using: V1::Entities::Users, unless: { include: :user_ads }
+      #expose :image, using: V1::Entities::Attachment
 
       def id
         object.id.to_s
       end
 
-      def profile_url
-        "http://#{ ENV['DOMAIN'] }/profile/#{ object.slug }"
-      end
-
-      def image_url
-        image = object.profile_image
-        case image.class.name
-        when 'Attachment' then image.url
-        when 'String'     then image end
-      end
-
-      def ads
-        object.active_ads.latest
+      def image
+        object.prospect_image
       end
     end
   end
