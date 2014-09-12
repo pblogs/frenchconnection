dae2bdd69f07aaaaf9a6d1931edc202e484ed5d1
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-09-12 15:03:55 +0200

    mount Entities::Tasks

--- app/api/v1/base.rb
+++ app/api/v1/base.rb
@@ -1,9 +1,9 @@
-#require 'v1/entities/users'
 module V1
   class Base < Grape::API
     version 'v1', using: :path, vendor: 'orwapp', cascade: false
 
     mount Users
+    mount V1::Entities::Tasks
     #mount V1::Entities::Users
 
   end
