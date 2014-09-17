1ea32b6602694956bc8125b0032231ebf9341183
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-09-17 14:00:23 +0200

    a Task exposes customer_url and project_url - TODO: Build the URLs as shown on line 11,12

--- app/api/v1/entities/tasks.rb
+++ app/api/v1/entities/tasks.rb
@@ -3,11 +3,23 @@ module V1
 
     class Tasks < Grape::Entity
       expose :id, :description, :project_id, :project_url
+
+      def project_url
+        project_id = object.project.id
+        "http://#{ ENV['DOMAIN'] || 'alliero-orwapp.dev' }" +
+        "/projects/#{project_id}/tasks/#{object.id}/"
+        # customer_project_path(object.project.customer, 
+        # object.project)
+      end
+
+      def customer_url
+        "http://#{ ENV['DOMAIN'] || 'alliero-orwapp.dev' }" +
+          "/customers/#{object.project.customer.id}"
+        # customer_project_path(object.project.customer, 
+        # object.project)
+      end
     end
 
-    def project_url
-      'http://db.no/'
-    end
 
   end
 end
