c525d316d43deaf40a85cb46c7518d61a9b8ec43
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-09-19 23:53:44 +0200

    cleanup projects entities

--- app/api/v1/entities/projects.rb
+++ app/api/v1/entities/projects.rb
@@ -1,23 +1,7 @@
 module V1
   module Entities
-
     class Projects < Grape::Entity
-      expose :id, :description, :project_id, :project_url
-
-      def project_url
-        project_id = object.project.id
-        "http://#{ ENV['DOMAIN'] || 'alliero-orwapp.dev' }" +
-        "/projects/#{project_id}/tasks/#{object.id}/"
-        # customer_project_path(object.project.customer, 
-        # object.project)
-      end
-
-      def customer_url
-        "http://#{ ENV['DOMAIN'] || 'alliero-orwapp.dev' }" +
-          "/customers/#{object.project.customer.id}"
-      end
-
+      expose :id, :name, :description
     end
-
   end
 end
