6167b4ed0ade7d735960016ee980174c39aa70b9
andersbn <andersbn@gmail.com>
2014-09-21 23:33:55 +0200

    More modifications post code review.

--- app/api/v1/entities/tasks.rb
+++ app/api/v1/entities/tasks.rb
@@ -4,7 +4,20 @@ module V1
     class Tasks < Grape::Entity
       expose :id, :description, :project_id
 
+      def project_url
+        project_id = object.project.id
+        "http://#{ ENV['DOMAIN'] || 'alliero-orwapp.dev' }" +
+        "/projects/#{project_id}/tasks/#{object.id}/"
+        # customer_project_path(object.project.customer, 
+        # object.project)
+      end
+
+      def customer_url
+        "http://#{ ENV['DOMAIN'] || 'alliero-orwapp.dev' }" +
+          "/customers/#{object.project.customer.id}"
+      end
     end
-    
+
+
   end
 end
