77b601370c06b52dd16fbb8c9dcc05810d42a59e
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2014-09-22 12:56:38 +0100

    #6 added DISTINCT to query

--- app/workers/complete_project_worker.rb
+++ app/workers/complete_project_worker.rb
@@ -3,7 +3,7 @@ class CompleteProjectWorker
 
   def perform(project_id)
     project = Project.find(project_id)
-    project.users.workers.each do |worker|
+    project.users.workers.distinct.each do |worker|
       Sms.send_msg(worker.mobile, I18n.t('sms.complete_project', project_name: project.name))
     end
   end
