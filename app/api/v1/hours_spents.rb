d7efc62b76c0ca3d3ffed83ffb68e6a744fc5d00
andersbn <andersbn@gmail.com>
2014-09-28 21:01:16 +0200

    Refactor api

--- app/api/v1/hours_spents.rb
+++ app/api/v1/hours_spents.rb
@@ -3,6 +3,30 @@ module V1
 
     resource :hours_spents do
       
+      desc "Create HoursSpents by user on task on date"
+      options 'users/:user_id/tasks/:task_id/dates/:date' do
+        header 'Access-Control-Allow-Headers', 'Content-Type'
+        header 'Access-Control-Allow-Origin', '*'
+      end
+      
+      post 'users/:user_id/tasks/:task_id/dates/:date' do
+        request_params = ActionController::Parameters.new(params)
+        permitted_params = request_params.permit(:user_id, :task_id, :date,
+          :hour, :overtime_50, :overtime_100, :description,
+          :runs_in_company_car, :km_driven_own_car, :toll_expenses_own_car,
+          :supplies_from_warehouse)
+        
+        if hours_spent = HoursSpent.create(permitted_params)
+          hours_spent.project_id = Task.find(hours_spent.task_id).project_id
+          hours_spent.save!
+          present hours_spent.id
+          header 'Access-Control-Allow-Origin', '*'
+        else
+          error! hours_spent.errors, 400
+          header 'Access-Control-Allow-Origin', '*'
+        end
+      end
+      
       desc "Update HoursSpents with given id"
       params do
         requires :hours_spent_id, type: Integer, desc: "HoursSpent id."
@@ -13,7 +37,6 @@ module V1
           header 'Access-Control-Allow-Methods', 'PUT'
           header 'Access-Control-Allow-Origin', '*'
         end
-        
         put do
           hours_spent = HoursSpent.find(params[:hours_spent_id])
           
@@ -31,7 +54,46 @@ module V1
             header 'Access-Control-Allow-Origin', '*'
           end
         end
+      end
+      
+      desc "HoursSpents by user"
+      get ':user_id' do
+        user = User.find(params[:user_id])
+        hours_spents = user.hours_spents
+        present :hours_spents, hours_spents, with: V1::Entities::HoursSpents
+        header 'Access-Control-Allow-Origin', '*'
+      end
+      
+      desc "HoursSpents by user on task"
+      get 'users/:user_id/tasks/:task_id' do
+        user = User.find(params[:user_id])
+        task_id = params[:task_id].to_i
+        hours_spents_on_task = 
+          user.hours_spents.select { 
+            |hour_spent| hour_spent.task_id == task_id }
+          
+        present :hours_spents, 
+                hours_spents_on_task, 
+                with: V1::Entities::HoursSpents
         
+        header 'Access-Control-Allow-Origin', '*'
+      end
+      
+      desc "HoursSpents by user on task on date"
+      get 'users/:user_id/tasks/:task_id/dates/:date' do
+        user = User.find(params[:user_id])
+        task_id = params[:task_id].to_i
+        date = params[:date]
+        hours_spents_on_task_on_date = 
+          user.hours_spents.select { 
+            |hour_spent|  hour_spent.task_id == task_id && 
+                          hour_spent.date.to_s == date }
+            
+        present :hours_spents, 
+                hours_spents_on_task_on_date, 
+                with: V1::Entities::HoursSpents
+        
+        header 'Access-Control-Allow-Origin', '*'
       end
 
     end
