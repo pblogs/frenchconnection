51cb49e8a497532429d35b0504012ef55afd0bf4
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2014-09-29 11:57:13 +0200

    #14 send zipped report in a proper way

--- app/controllers/project_reports_controller.rb
+++ app/controllers/project_reports_controller.rb
@@ -2,7 +2,8 @@ class ProjectReportsController < ApplicationController
   def daily_report
     project = Project.find(params[:id])
     if report = ZippedReport.where(project: project).order(:created_at).last
-      send_file report.zipfile.path
+      send_data report.zipfile.read,
+                filename: File.basename(report.zipfile.path)
     else
       render text: "Report not ready"
     end
