2182af7a13af2348edecf5436d832db706a8cf1a
andersbn <andersbn@gmail.com>
2014-10-02 23:54:08 +0200

    did not help. put back endpoint for GET workers

--- spec/api/users_spec.rb
+++ spec/api/users_spec.rb
@@ -2,19 +2,19 @@ require 'spec_helper'
 
 describe V1::Users do
   
-  #describe 'GET /api/v1/users/workers' do
-  #  it 'lists all workers' do
-  #    user1 = Fabricate(:user, roles: [:worker])
-  #    user2 = Fabricate(:user, roles: [:worker])
-  #    user3 = Fabricate(:user, roles: [:worker, :project_leader])
-  #    user4 = Fabricate(:user, roles: [:project_leader])
-  #    get "/api/v1/users/workers"
-  #    response.status.should == 200
-  #    hash = JSON.parse(response.body)
-  #    worker_ids = hash['users'].collect { |u| u['id'] }
-  #    worker_ids.should =~ [user3.id, user2.id, user1.id] 
-  #  end
-  #end
+  describe 'GET /api/v1/users/workers' do
+    it 'lists all workers' do
+      user1 = Fabricate(:user, roles: [:worker])
+      user2 = Fabricate(:user, roles: [:worker])
+      user3 = Fabricate(:user, roles: [:worker, :project_leader])
+      user4 = Fabricate(:user, roles: [:project_leader])
+      get "/api/v1/users/workers"
+      response.status.should == 200
+      hash = JSON.parse(response.body)
+      worker_ids = hash['users'].collect { |u| u['id'] }
+      worker_ids.should =~ [user3.id, user2.id, user1.id] 
+    end
+  end
 
   describe 'GET /api/v1/users/id/:user_id' do
     it 'lists a specific user' do
