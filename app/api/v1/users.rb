b18752113983c25ff826666cda6a2baf71c47186
andersbn <andersbn@gmail.com>
2014-10-02 23:35:56 +0200

    test remove endpoint for GET workers

--- app/api/v1/users.rb
+++ app/api/v1/users.rb
@@ -3,22 +3,22 @@ module V1
 
     resource :users do
       
-      desc "All users with worker role"
-      get 'workers' do
-        workers = User.select { |user| user.roles.include? 'worker' }
-        present :users, workers, with: V1::Entities::Users
-        header 'Access-Control-Allow-Origin', '*'
-      end
-
-      #desc "One user"
-      #params do
-      #  requires :user_id, type: Integer, desc: "User id."
-      #end
-      #get 'id/:user_id' do
-      #  user = User.find(params[:user_id])
-      #  present :user, user, with: V1::Entities::Users
+      #desc "All users with worker role"
+      #get 'workers' do
+      #  workers = User.select { |user| user.roles.include? 'worker' }
+      #  present :users, workers, with: V1::Entities::Users
       #  header 'Access-Control-Allow-Origin', '*'
       #end
+
+      desc "One user"
+      params do
+        requires :user_id, type: Integer, desc: "User id."
+      end
+        get 'id/:user_id' do
+          user = User.find(params[:user_id])
+        present :user, user, with: V1::Entities::Users
+        header 'Access-Control-Allow-Origin', '*'
+      end
       
     end
   end
