23d86ae8520fcc3673b172253857bfade1c0f223
andersbn <andersbn@gmail.com>
2014-10-02 23:57:39 +0200

    fix indentation mistake

--- app/api/v1/users.rb
+++ app/api/v1/users.rb
@@ -14,8 +14,8 @@ module V1
       params do
         requires :user_id, type: Integer, desc: "User id."
       end
-        get 'id/:user_id' do
-          user = User.find(params[:user_id])
+      get 'id/:user_id' do
+        user = User.find(params[:user_id])
         present :user, user, with: V1::Entities::Users
         header 'Access-Control-Allow-Origin', '*'
       end
