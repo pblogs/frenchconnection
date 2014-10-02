075d112d85ff96f59ec466f5264df1d344208546
andersbn <andersbn@gmail.com>
2014-10-02 23:18:44 +0200

    test remove endpoint for GET single user

--- app/api/v1/users.rb
+++ app/api/v1/users.rb
@@ -10,15 +10,15 @@ module V1
         header 'Access-Control-Allow-Origin', '*'
       end
 
-      desc "One user"
-      params do
-        requires :user_id, type: Integer, desc: "User id."
-      end
-      get 'id/:user_id' do
-        user = User.find(params[:user_id])
-        present :user, user, with: V1::Entities::Users
-        header 'Access-Control-Allow-Origin', '*'
-      end
+      #desc "One user"
+      #params do
+      #  requires :user_id, type: Integer, desc: "User id."
+      #end
+      #get 'id/:user_id' do
+      #  user = User.find(params[:user_id])
+      #  present :user, user, with: V1::Entities::Users
+      #  header 'Access-Control-Allow-Origin', '*'
+      #end
       
     end
   end
