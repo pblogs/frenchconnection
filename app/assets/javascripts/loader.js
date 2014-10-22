b0cad9ab5f3680b2615183b993fcc7f4174b2005
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2014-10-22 15:04:16 +0200

    Restrict available dates for new tasks to be within the project start and due date #43

--- app/assets/javascripts/loader.js
+++ app/assets/javascripts/loader.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
-  $(".datepicker").datepicker({
-    dateFormat: "dd.mm.yy"
+
+  /* allow additional options for datepicker, based on:
+  / http://stackoverflow.com/questions/8308008/jquery-datepicker-override-options-from-data-attribute
+  */
+  $(".datepicker").each(function() {
+    //standard options
+    var options = { dateFormat: "dd.mm.yy"};
+    //additional options
+    var additionalOptions = $(this).data("datepicker");
+    //merge the additional options into the standard options and override defaults
+    jQuery.extend(options, additionalOptions);
+
+    $(this).datepicker(options);
   });
 });
