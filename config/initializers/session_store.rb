23d1f937a4364ca31ae4f0d6126faae891d4392e
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2014-11-18 12:29:10 +0100

    Revert "share cookies across subdomains"
    This reverts commit 055d6f21592245115e4849957dfd58d7e1e7dcb9.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-AllieroForms::Application.config.session_store :cookie_store,
-                                               key: '_AllieroForms_session',
-                                               domain: '.orwapp.com'
+AllieroForms::Application.config.session_store :cookie_store, key: '_AllieroForms_session', domain: :all
+
