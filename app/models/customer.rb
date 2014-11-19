c016ceb6a86730bb351dbe7e24f8b1b43413fb7f
Darjan Vukusic <darjan.vukusic@gmail.com>
2014-11-19 15:35:21 +0100

    remove customer_nr validation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -18,7 +18,6 @@
 
 class Customer < ActiveRecord::Base
   validates :name, presence: true
-  validates :customer_nr, uniqueness: true
 
   has_many :tasks, dependent: :destroy
   has_many :projects, dependent: :destroy
