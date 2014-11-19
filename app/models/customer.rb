c21b1083ed8ba2b249c3ec1918c5d79a2fa50dbd
Darjan Vukusic <darjan.vukusic@gmail.com>
2014-11-19 14:41:16 +0100

    paginate customers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -17,8 +17,8 @@
 #
 
 class Customer < ActiveRecord::Base
-  #validates :name,           :presence => true
-  validates :customer_nr, :uniqueness => true
+  validates :name, presence: true
+  validates :customer_nr, uniqueness: true
 
   has_many :tasks, dependent: :destroy
   has_many :projects, dependent: :destroy
