a691162584f38adf71ba6f0ee559b1283c413ae6
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-12-03 15:05:53 +0100

    test

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -5,6 +5,10 @@ describe Inventory do
   let(:bulldozer) { Fabricate(:inventory, name: 'Big Cat', 
                               certificates: [buldoze_driver]) }
 
+  it 'is valid from the fabric' do
+    Fabricate(:inventory).should be_valid
+  end
+
   it 'requires sertificates' do
     bulldozer.certificates.should eq [buldoze_driver]
   end
