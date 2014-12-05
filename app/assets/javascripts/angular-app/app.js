561d04c68d3896af4a01df81000e8f76c78ff9c6
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-12-05 20:41:15 +0100

    Running with both ‘app’ and @App till I find out.. how to get rid of ‘app’.

--- app/assets/javascripts/angular-app/app.js
+++ app/assets/javascripts/angular-app/app.js
@@ -1,12 +1,2 @@
 angular
-  .module('app', ['ngRoute', 'restangular'])
-  .config(['$routeProvider', 'RestangularProvider', function($routeProvider, RestangularProvider) {
-    RestangularProvider.setBaseUrl('/api/v1');
-    // $routeProvider.when('/', {
-    //   controller: 'MainCtrl'
-    // }).when('/:status', {
-    //   controller: 'HomeCtrl',
-    // }).otherwise({
-    //   redirectTo: '/'
-    // });
-  }]);
+  .module('app', ['ngRoute' ])
