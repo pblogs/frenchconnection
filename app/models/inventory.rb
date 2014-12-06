18de9d041183a47cb2704d4124721ad6c27090e7
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-12-06 15:23:32 +0100

    A task can have many inventories.

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -2,4 +2,5 @@ class Inventory < ActiveRecord::Base
   has_and_belongs_to_many :certificates
   validates :name, uniqueness: true
   validates :name, presence: true
+  has_and_belongs_to_many :tasks
 end
