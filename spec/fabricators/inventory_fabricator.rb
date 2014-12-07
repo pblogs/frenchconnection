b9115b14cc2c5d706694edd5f00ff85e78b0a525
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-12-07 10:29:05 +0100

    added Inventories to the API

--- spec/fabricators/inventory_fabricator.rb
+++ spec/fabricators/inventory_fabricator.rb
@@ -1,6 +1,7 @@
 Fabricator(:inventory) do
-  name                             "Bulldozer"
+  name                             { Faker::Lorem.word }
   description                      "5 tons of Cat"
   can_be_rented_by_other_companies false
   rental_price_pr_day              1
 end
+
