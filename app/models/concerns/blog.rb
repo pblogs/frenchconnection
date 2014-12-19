2998b2204659946e4eb2e8442902009037475675
Darjan Vukusic <darjan.vukusic@gmail.com>
2014-12-19 11:14:50 +0100

    allow published without publish date

--- app/models/concerns/blog.rb
+++ app/models/concerns/blog.rb
@@ -2,8 +2,10 @@ module Blog
   extend ActiveSupport::Concern
 
   included do
-    scope :published, -> { where('published = :published and publish_date <=
-                                :date', published: true, date: Date.today)
-                          .order(publish_date: :desc).order(updated_at: :desc) }
+    scope :published, -> { where('published = :published AND
+                            (publish_date <= :date OR publish_date is null)',
+                            published: true, date: Date.today)
+                              .order(publish_date: :desc)
+                              .order(updated_at: :desc) }
   end
 end
