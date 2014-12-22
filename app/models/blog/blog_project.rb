450e347800d4160de09d1334f07731810dab3991
Darjan Vukusic <darjan.vukusic@gmail.com>
2014-12-22 17:54:49 +0100

    annotate

--- app/models/blog/blog_project.rb
+++ app/models/blog/blog_project.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: blog_projects
+#
+#  id           :integer          not null, primary key
+#  title        :string(255)
+#  content      :text
+#  image        :string(255)
+#  locale       :string(255)
+#  published    :boolean
+#  publish_date :date
+#  created_at   :datetime
+#  updated_at   :datetime
+#
+
 class BlogProject < ActiveRecord::Base
   include Blog
 
--- spec/models/blog_article_spec.rb
+++ spec/models/blog_article_spec.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: blog_articles
+#
+#  id           :integer          not null, primary key
+#  title        :string(255)
+#  content      :text
+#  image        :string(255)
+#  locale       :string(255)
+#  published    :boolean
+#  publish_date :date
+#  created_at   :datetime
+#  updated_at   :datetime
+#
+
 require 'spec_helper'
 
 describe BlogArticle do
