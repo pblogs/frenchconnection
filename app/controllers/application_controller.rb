1fbddbe91903816e12502047082fe84530ba06de
andersbn <andersbn@gmail.com>
2014-12-23 12:05:46 +0100

    added rack/cors and responds to json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
   before_action :configure_permitted_parameters, if: :devise_controller?
   
 
   before_action :set_current_user
   before_action :authenticate_user!
+  
+  respond_to :html, :json
 
   def set_current_user
     @current_user ||= current_user
