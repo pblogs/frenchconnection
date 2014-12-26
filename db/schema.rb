e41ce97d3100378ba1f4997ba3610277d2c2c173
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-12-26 10:46:33 +0100

    drop work_categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141216150615) do
+ActiveRecord::Schema.define(version: 20141226094405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -424,12 +424,6 @@ ActiveRecord::Schema.define(version: 20141216150615) do
 
   add_index "users", ["profession_id"], name: "index_users_on_profession_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
-  create_table "work_categories", force: true do |t|
-    t.string   "title"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "zipped_reports", force: true do |t|
     t.integer  "project_id",  null: false
