9a98fedf27db5786c0dd8db86230cad6d2091d7f
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-12-30 12:26:31 +0100

    fix test

--- spec/controllers/certificates_controller_spec.rb
+++ spec/controllers/certificates_controller_spec.rb
@@ -23,7 +23,13 @@ describe CertificatesController do
   # This should return the minimal set of attributes required to create a valid
   # Certificate. As you add validations to Certificate, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { "title" => "MyString" } }
+  let(:valid_attributes) do
+    Fabricate.build(:certificate).serializable_hash
+  end
+
+  before do
+    sign_in
+  end
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
