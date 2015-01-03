3da02e3739e983ef601f6d2ccac067a10b226f0b
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-01-03 10:53:30 +0100

    don’t update with a bang

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -75,7 +75,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
-      if @user.update!(user_params)
+      if @user.update(user_params)
         format.html { redirect_to users_url,
                     notice: 'User was successfully updated.' }
         format.json { head :no_content }
@@ -118,9 +118,9 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet,
     #only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :certificate_ids,
-                                  :tasks_id, :mobile, :emp_id, :profession_id,
-                                  :department_id, :image, roles: [])
+      params.require(:user).permit(:first_name, :last_name, 
+         :tasks_id, :mobile, :emp_id, :profession_id,
+        :department_id, :image, roles: [], :certificate_ids => [])
     end
 
 end
