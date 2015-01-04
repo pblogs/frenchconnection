c9e67c6f0db35a0c6cbe5d97227d67c619b70a32
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-01-04 15:26:14 +0100

    more thorough test

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -6,17 +6,23 @@ RSpec.configure do |c|
 end
 
 feature "Task" do
-  scenario "Create a new task", js: true do
-    @project_leader = Fabricate(:user, roles: [:project_leader])
-    project_start_date = '01.01.2014'
-    @project        = Fabricate(:project, start_date: project_start_date)
-    sign_in(@project_leader)
+  before do
+    @project_leader     = Fabricate(:user, roles: [:project_leader])
+    @project_start_date = '01.01.2014'
+    @project            = Fabricate(:project, start_date: @project_start_date)
+    @welding            = Fabricate(:skill, title: 'welding')
+    @location           = Fabricate(:location, name: 'roof-top')
+  end
 
+  scenario "Create a new task", js: true do
+    sign_in(@project_leader)
     visit customer_project_path(@project.customer, @project)
     expect {
       click_link I18n.t('register_new_task')
+      check  I18n.t('skills.welding')
+      choose I18n.t('locations.roof-top')
       fill_in Task.human_attribute_name("description"), with: 'bra prosjekt'
-      fill_in Task.human_attribute_name("start_date"),  with: project_start_date
+      fill_in Task.human_attribute_name("start_date"),  with: @project_start_date
       click_link I18n.t('continue')
     }.to change(Task, :count).by(1)
   end
