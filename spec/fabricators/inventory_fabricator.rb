dbaab8fee6e235517bbaa71fa4c2bc4dcc7ff15d
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-01-11 15:44:22 +0100

    update test

--- spec/fabricators/inventory_fabricator.rb
+++ spec/fabricators/inventory_fabricator.rb
@@ -1,5 +1,5 @@
 Fabricator(:inventory) do
-  name                             { Faker::Lorem.word }
+  name                             { Faker::Lorem.sentences }
   description                      "5 tons of Cat"
   can_be_rented_by_other_companies false
   rental_price_pr_day              1
