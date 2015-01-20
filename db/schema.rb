f2d4c1826b2a99553ddaf974613dd2585c873c3b
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-01-20 10:41:56 +0100

    resolve gem and jquery-ui conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,21 @@ ActiveRecord::Schema.define(version: 20150107183914) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "active_admin_comments", force: true do |t|
+    t.string   "namespace"
+    t.text     "body"
+    t.string   "resource_id",   null: false
+    t.string   "resource_type", null: false
+    t.integer  "author_id"
+    t.string   "author_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
+  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
+  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
+
   create_table "api_keys", force: true do |t|
     t.string  "name"
     t.string  "access_token"
@@ -42,6 +57,48 @@ ActiveRecord::Schema.define(version: 20150107183914) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "description"
+  end
+
+  create_table "blog_articles", force: true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.string   "image"
+    t.string   "locale"
+    t.boolean  "published"
+    t.date     "publish_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "blog_images", force: true do |t|
+    t.string   "image"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "owner_type"
+    t.integer  "owner_id"
+  end
+
+  create_table "blog_projects", force: true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.string   "image"
+    t.string   "locale"
+    t.boolean  "published"
+    t.date     "publish_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "blog_videos", force: true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.string   "video_url"
+    t.string   "locale"
+    t.boolean  "published"
+    t.date     "publish_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "certificates", force: true do |t|
