0069992a2f9d61c0c685f7d0b11ceddefff23651
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-01-20 13:34:00 +0100

    fetch blog things from 11ffb

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -14,36 +14,28 @@ class BlogController < ApplicationController
 
   def video
     @current = params[:id].present? ?
-      BlogVideo.find_by(id: params[:id], published: true) :
-      BlogVideo.published.first
+        BlogVideo.find_by(id: params[:id], published: true) :
+        BlogVideo.published.first
     @blog_videos = BlogVideo.published
-      .where('id != :id', id: @current.id) if @current.present?
+                    .where('id != :id', id: @current.id) if @current.present?
   end
 
   def hms
   end
 
-  def admin
-  end
-
-  def administration
-  end
-
-
   def instructions
     @url = 'http://home.powertech.no/martins/orwapp-assets/'
-    @files = {
-      '07.02_instruks_byggeplass.pdf'    => I18n.t('instructions.construction_site'),
-      '07.09_instruks_stillas.pdf'       => I18n.t('instructions.scaffold'),
-      '07.19_instruks_tvangsblander.pdf' => I18n.t('instructions.concrete_mixer'),
-      '07.20_instruks_gjerdesag.pdf'     => I18n.t('instructions.gjerdesag'),
-      '07.21_instruks_boltpistol.pdf'    => I18n.t('instructions.boltpistol'),
-      '07.22_instruks_vinkelsliper.pdf'  => I18n.t('instructions.vinkelsliper'),
-      '07.29_instruks_spikerpistol.pdf'  => I18n.t('instructions.spikerpistol'),
-      '07.30_instruks_kjedesag.pdf'      => I18n.t('instructions.kjedesag'),
-      '07.32_instruks_sikkerhetssele_fallblokk.pdf' => I18n.t('instructions.sikkerhetssele_fallblokk'),
-      '07.33_instruks_hoystrykksspyler.pdf'         => I18n.t('instructions.hoystrykksspyler')
-    }
+    @files = {'07.02_instruks_byggeplass.pdf' => I18n.t('instructions.construction_site'),
+              '07.09_instruks_stillas.pdf' =>    I18n.t('instructions.scaffold'),
+              '07.19_instruks_tvangsblander.pdf' => I18n.t('instructions.concrete_mixer'),
+              '07.20_instruks_gjerdesag.pdf'     => I18n.t('instructions.gjerdesag'),
+              '07.21_instruks_boltpistol.pdf' =>    I18n.t('instructions.boltpistol'),
+              '07.22_instruks_vinkelsliper.pdf'  =>  I18n.t('instructions.vinkelsliper'),
+              '07.29_instruks_spikerpistol.pdf' =>   I18n.t('instructions.spikerpistol'),
+              '07.30_instruks_kjedesag.pdf'     =>   I18n.t('instructions.kjedesag'),
+              '07.32_instruks_sikkerhetssele_fallblokk.pdf' => I18n.t('instructions.sikkerhetssele_fallblokk'), 
+              '07.33_instruks_hoystrykksspyler.pdf' => I18n.t('instructions.hoystrykksspyler')
+              }
   end
 
   def frontpage_manager
@@ -57,7 +49,7 @@ class BlogController < ApplicationController
 
   def get_content
     type = params[:content_type].present? ?
-      params[:content_type].classify.constantize : nil
+        params[:content_type].classify.constantize : nil
     type.find(params[:id]) rescue nil
   end
 
