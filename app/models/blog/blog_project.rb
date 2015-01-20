c3b00f1ee1950a23fcebf55c5b373894a5bed636
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-01-20 13:34:00 +0100

    fetch blog things from 11ffb

--- app/models/blog/blog_project.rb
+++ app/models/blog/blog_project.rb
@@ -22,6 +22,4 @@ class BlogProject < ActiveRecord::Base
   has_many :blog_images, as: :owner, dependent: :destroy
 
   accepts_nested_attributes_for :blog_images, allow_destroy: true
-
-  mount_uploader :image, DocumentUploader
 end
