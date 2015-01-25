e09247981c5eda39d4959623b79b368997b59d20
andersbn <andersbn@gmail.com>
2015-01-25 20:18:03 +0100

    minor modifications post code review

--- app/api/v1/blog_articles.rb
+++ app/api/v1/blog_articles.rb
@@ -1,21 +1,16 @@
 module V1
   class BlogArticles < Base
-
-    resource :blog_articles do
-      
+    resource :blog_articles do 
       desc "Get all BlogArticles"
       get do
-        published_articles = BlogArticle.all.select {
-          |article| article.published
-        }
+        published_articles = 
+          BlogArticle.where(published: true).all
 
         present :blog_articles, published_articles, 
                 with: V1::Entities::BlogArticles
         
         header 'Access-Control-Allow-Origin', '*'
-      end
-      
+      end  
     end
-    
   end
 end
