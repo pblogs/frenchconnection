5f38530908687a1ca32582b082a2872fca7f7cac
andersbn <andersbn@gmail.com>
2015-01-25 20:18:03 +0100

    minor modifications post code review

--- app/api/v1/blog_projects.rb
+++ app/api/v1/blog_projects.rb
@@ -1,21 +1,16 @@
 module V1
   class BlogProjects < Base
-
-    resource :blog_projects do
-      
+    resource :blog_projects do  
       desc "Get all published BlogProcjects"
       get do
-        published_projects = BlogProject.all.select {
-          |project| project.published
-        }
+        published_projects =
+          BlogProject.where(published: true).all
 
         present :blog_projects, published_projects, 
                 with: V1::Entities::BlogProjects
         
         header 'Access-Control-Allow-Origin', '*'
-      end
-      
+      end 
     end
-    
   end
 end
