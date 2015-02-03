4831e42fbe39cf16de729717b6b193de610bba36
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-02-03 19:43:05 +0100

    no need for billable hours until the task is finished.

--- db/migrate/20150203140211_make_all_existing_hours_spent_personal.rb
+++ db/migrate/20150203140211_make_all_existing_hours_spent_personal.rb
@@ -3,8 +3,6 @@ class MakeAllExistingHoursSpentPersonal < ActiveRecord::Migration
   def change
     HoursSpent.where(of_kind: nil).all.each do |personal| 
       personal.update_attribute(:of_kind, 'personal') 
-      billable = personal.dup
-      billable.update_attributes(personal_id: personal.id, of_kind: 'billable')
     end
   end
 
