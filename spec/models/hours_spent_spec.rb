2b4bda62a77e717e7b11a8d0772a798703657e9c
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-02-03 10:41:36 +0100

    remove change test

--- spec/models/hours_spent_spec.rb
+++ spec/models/hours_spent_spec.rb
@@ -54,25 +54,6 @@ describe HoursSpent do
       @hours_spent.sum.should eq 100
     end
 
-    it 'can have one change' do
-      @change = Change.create_from_hours_spent(hours_spent: @hours_spent)
-      @change.save
-      @hours_spent.reload
-      @hours_spent.change.should eq @change
-    end
-
-
-
-    it 'changed_value(:type_of_hour)' do
-      # OBS: This has failed randomly earlier. 
-      @change = Change.create_from_hours_spent(hours_spent: @hours_spent)
-      @change.update_attribute(:overtime_50, 5)
-      @change.update_attribute(:overtime_100, 100)
-      @change.reload
-      @hours_spent.reload
-      @hours_spent.changed_value(:overtime_50).should eq 5
-      @hours_spent.changed_value(:overtime_100).should eq 100
-    end
 
     it 'week_numbers_for_dates(dates)' do
       dates = [Date.parse('01-01-2014'), Date.parse('08-01-2014'), 
