f28b205e7ac1d66fd248382d6ee5ac6aaee48e60
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-02-10 13:24:13 +0100

    added users/:user_id/hours

--- config/routes.rb
+++ config/routes.rb
@@ -69,6 +69,8 @@ AllieroForms::Application.routes.draw do
   resources :users do 
     get '/tasks/started'     => 'users/tasks#started'
     get '/tasks/not_started' => 'users/tasks#not_started'
+    get '/projects/:project_id/hours'=> 'users#hours', as: :hours
+    post '/projects/:project_id/hours'=> 'users#hours', as: :approve_hours
     resources :tasks, :controller => 'users/tasks' do
       post :accept_task
       post :finished
