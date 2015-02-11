93c5dcba6743fbf6e61bf10db469b3a3ee8cc90e
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-02-11 12:27:35 +0100

    registert_hours_spec

--- spec/features/register_hours_spec.rb
+++ spec/features/register_hours_spec.rb
@@ -1,29 +1,36 @@
 require 'spec_helper'
+require 'helpers'
 
-describe "Registered hours on a Task" do
+RSpec.configure do |c|
+    c.include Helpers
+end
+
+feature "Registered hours on a Task" do
   before :each do
-    @user = Fabricate(:user, first_name: 'Josh')
-    Fabricate(:paint, title: 'Acryl')
-    Fabricate(:paint, title: 'Beis')
+    @user       = Fabricate(:user)
     @sporveiene = Fabricate(:customer, name: 'Oslo Sporveier AS')
-    @project = Fabricate(:project, customer: @sporveiene)
-    @task = Fabricate(:task)
+    @project    = Fabricate(:project, customer: @sporveiene)
+    @task       = Fabricate(:task, project: @project)
     @user.tasks << @task
+    @hour       = Fabricate(:hours_spent, task: @task, hour: 10, user: @user)
+    @project_leader     = Fabricate(:user, roles: [:project_leader])
   end
 
-  pending "An user can register hours on the tasks he is delegated" do
-    visit user_path(@user)
-    HoursSpent.all.size.should eq 0
-    click_link "Aktive oppdrag"
-    click_link "Registrer timer"
-    fill_in 'Akkord', with: 20
-    fill_in '50%', with: 50
-    fill_in 'Beskrivelse', with: 'Malt begge sider av veggen.'
-    click_button 'Lagre'
-    HoursSpent.all.size.should eq 1
-    h = HoursSpent.last
-    h.piecework_hours.should eq 20
-    h.overtime_50.should eq 50
-    h.description.should eq 'Malt begge sider av veggen.'
+  context 'As an admin' do
+    scenario "edit existing hours" do
+      # Check total hours for project
+      expect(@project.hours_spent_total(overtime: :hour)).to eq 10
+
+      sign_in(@project_leader)
+      visit user_hours_path(@user, @project)
+      click_link 'edit_hour'
+      fill_in HoursSpent.human_attribute_name("change_reason"),
+        with: 'feil timer ført'
+      fill_in HoursSpent.human_attribute_name("hour"), with: '111'
+      fill_in HoursSpent.human_attribute_name("overtime_50"), with: '555'
+      click_link_or_button I18n.t('save')
+      expect(@project.hours_spent_total(overtime: :hour)).to eq 111
+    end
   end
 end
+
