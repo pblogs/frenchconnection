5b3f720ead32726cd48270b23b2714ef9211d5e8
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-02-16 17:57:55 +0100

    > 80 chars

--- spec/models/zipped_report_spec.rb
+++ spec/models/zipped_report_spec.rb
@@ -16,14 +16,15 @@ describe ZippedReport do
   describe "cleanup_old_reports" do
 
     before do
-      @report = Fabricate :zipped_report
+      @report       = Fabricate :zipped_report
       @other_report = Fabricate :zipped_report
     end
 
     it "cleans up properly" do
       fresh_report = ZippedReport.create(
           project: @report.project, report_type: @report.report_type)
-      expect { fresh_report.cleanup_old_reports }.to change(ZippedReport, :count).by(-1)
+      expect { fresh_report.cleanup_old_reports 
+      }.to change(ZippedReport, :count).by(-1)
     end
 
   end
