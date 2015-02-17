85a797a054b7df318ccd394a32a3305b81d8d666
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-02-17 18:33:38 +0100

    add year as search param for HoursSpent

--- app/controllers/project_reports_controller.rb
+++ app/controllers/project_reports_controller.rb
@@ -8,11 +8,14 @@ class ProjectReportsController < ApplicationController
 
   def create
     @project = Project.find(params[:project_id])
+    @month = params[:month]
+    @year  = params[:year]
     case type = params[:type]
       when 'daily_report', 'timesheet'
         worker = "#{type.camelize}Worker".constantize
         @token = SecureRandom::uuid
-        worker.perform_in(5.seconds.since, params[:project_id], current_user.id, @token)
+        worker.perform_in(5.seconds.since, params[:project_id],
+                          current_user.id, @token, @month, @year)
       else
         raise "Invalid report type: #{type}"
     end
