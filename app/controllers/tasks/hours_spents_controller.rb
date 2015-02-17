0ad9b1e823bb6fb554a293af5c892de3d4c49cf4
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-02-17 23:03:23 +0100

    no need to save after approve! runs update_attribute

--- app/controllers/tasks/hours_spents_controller.rb
+++ app/controllers/tasks/hours_spents_controller.rb
@@ -79,7 +79,7 @@ module Tasks
 
     def approve
       @hour.approve!
-      @hour.save!
+      #@hour.save!
       redirect_to user_hours_path(@hour.user, @hour.project,
                                   of_kind: params[:of_kind])
     end
