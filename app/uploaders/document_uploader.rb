1768b17c33d60a5f8789dce81ac4be9132f3359d
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-02-17 10:31:03 +0100

    remove support for image uploads and manipulations.

--- app/uploaders/document_uploader.rb
+++ app/uploaders/document_uploader.rb
@@ -1,12 +1,4 @@
-# encoding: utf-8
-
 class DocumentUploader < CarrierWave::Uploader::Base
-
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-
-  # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
     storage :fog  
   else
--- app/uploaders/zipfile_uploader.rb
+++ app/uploaders/zipfile_uploader.rb
@@ -1,10 +1,4 @@
-# encoding: utf-8
-
 class ZipfileUploader < CarrierWave::Uploader::Base
-
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
@@ -27,28 +21,5 @@ class ZipfileUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
-  # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
-
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
-
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
 
 end
