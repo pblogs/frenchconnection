fd160c0344176306d98af0a564aadb725018a10b
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-02-17 13:38:37 +0100

    > 80 chars and remove comments

--- app/uploaders/document_uploader.rb
+++ app/uploaders/document_uploader.rb
@@ -1,10 +1,4 @@
-# encoding: utf-8
-
 class DocumentUploader < CarrierWave::Uploader::Base
-
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
@@ -13,8 +7,6 @@ class DocumentUploader < CarrierWave::Uploader::Base
     storage :file
   end
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
@@ -22,6 +14,5 @@ class DocumentUploader < CarrierWave::Uploader::Base
   def cache_dir
     "#{Rails.root}/tmp/uploads"
   end
-
 
 end
