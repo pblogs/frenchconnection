6dd9eed174ba1e1df3339167bd7a1eea487d75dc
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-03-03 21:13:44 +0100

    Use FOG_DIRECTORY

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,14 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
-      :provider               => 'AWS',                        
+      :provider               => 'AWS',
       :aws_access_key_id      => ENV['STABENFELDT_AWS_ACCESS_KEY_ID'],
       :aws_secret_access_key  => ENV['STABENFELDT_SECRET_ACCESS_KEY'],
       :region                 => 'eu-west-1',
     }
-    config.fog_directory  = ENV['SHORT_NAME']                       # required
+    config.fog_directory  = ENV['FOG_DIRECTORY']                       # required
     config.fog_public     = false                                   # optional, defaults to true
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
   end
-  
+
 end
