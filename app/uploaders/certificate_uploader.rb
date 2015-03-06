6463b10122cfe7c256c97cb79fe02bd57f2e6689
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-03-06 10:57:20 +0100

    large version

--- app/uploaders/certificate_uploader.rb
+++ app/uploaders/certificate_uploader.rb
@@ -49,6 +49,10 @@ class CertificateUploader < CarrierWave::Uploader::Base
     process resize_to_fit: [400, 400]
   end
 
+  version :large do
+    process resize_to_fit: [900, 900]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
