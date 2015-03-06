19163caaecd881546a784ea790f2120ad65206f0
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-03-06 09:22:14 +0100

    skip validation for now

--- app/models/user_certificate.rb
+++ app/models/user_certificate.rb
@@ -1,8 +1,8 @@
 class UserCertificate < ActiveRecord::Base
   belongs_to :user
   belongs_to :certificate
-  validates :user_id, presence: true
-  validates :certificate_id, presence: true
+  #validates :user_id, presence: true
+  #validates :certificate_id, presence: true
 
   mount_uploader :image, CertificateUploader
 end
