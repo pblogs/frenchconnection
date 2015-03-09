1deeb45e522138bde4ca4b3667b9f1367a86b787
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-03-09 12:29:34 +0100

    added pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  include Pundit
   protect_from_forgery with: :null_session
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_current_user
   before_action :authenticate_user!
+
 
   respond_to :html, :json
 
