c31e253dc018492863825aff09bf8492f993d3fd
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-03-10 10:25:21 +0100

    custom error message if pundit throws an exception.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   protect_from_forgery with: :null_session
 
-  after_action :verify_authorized, :except => :index
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_current_user
   before_action :authenticate_user!
@@ -24,6 +25,15 @@ class ApplicationController < ActionController::Base
 
 
   private
+
+ def user_not_authorized(exception)
+   policy_name = exception.policy.class.to_s.underscore
+   flash[:error] = t "#{policy_name}.#{exception.query}",
+                   scope: "pundit", default: :default
+   redirect_to(request.referrer || root_path)
+ end
+
+
 
   def get_paginate_default_field(first_char)
     if first_char.can_be_integer?
