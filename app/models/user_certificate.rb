1927774294ed93e16e4ef872649ee394b34c2f1e
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-03-24 11:06:39 +0100

    validate presence

--- app/models/user_certificate.rb
+++ app/models/user_certificate.rb
@@ -1,8 +1,16 @@
 class UserCertificate < ActiveRecord::Base
   belongs_to :user
   belongs_to :certificate
-  #validates :user_id, presence: true
-  #validates :certificate_id, presence: true
+
+  validates :user_id,        presence: true
+  validates :certificate_id, presence: true
+  validates :expiry_date,    presence: true
+  validates :image,          presence: true unless Rails.env.test?
 
   mount_uploader :image, CertificateUploader
+
+  def title
+    certificate.title
+  end
+
 end
