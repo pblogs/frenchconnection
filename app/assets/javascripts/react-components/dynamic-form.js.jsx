46dc14d7bd43090455e0c1abc6ac220bc005b04c
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-04-01 13:35:20 +0200

    no syntax errors.

--- app/assets/javascripts/react-components/dynamic-form.js.jsx
+++ app/assets/javascripts/react-components/dynamic-form.js.jsx
@@ -8,36 +8,41 @@ $( document ).ready( function()  {
     title             : null
   }
 
+
   var DynamicForm = React.createClass({
+    //getDefaultProps: function() {
+    //  pop_vals = [:automatically, :web_start, :web_end]
+    //  return { populate_at: pop_vals }
+    //}
+
     render: function() {
       return (
         <div>
           <label>Name</label>
-          <input type="text" ref="name" defaultValue={this.props.fieldValues.name} />
+          <input type="text" ref="name" defaultValue='hi' />
+          <Select data={this.props.populate_at} />
           <button onClick={this.saveAndContinue}>Save and Continue</button>
         </div>
       );
-    }
-
-  });
+    },
     saveAndContinue: function(e) {
-      e.preventDefault();
+      e.preventDefault()
 
-      //// Get values via this.refs
-      //var data = {
-      //  name     : this.refs.name.getDOMNode().value,
-      //  password : this.refs.password.getDOMNode().value,
-      //  email    : this.refs.email.getDOMNode().value,
-      //}
+      // Get values via this.refs
+      var data = {
+        name     : this.refs.name.getDOMNode().value,
+      }
+      console.log('data: ' + data['name'])
 
-      //this.props.saveValues(data)
-      //this.props.nextStep()
     }
+  })
 
 
-  //var dynamic_form = document.getElementById('dynamic-form')
-  //if ( dynamic_form ) {
-  //  React.render(<DynamicForm />, dynamic_form)
-  //};
+  var mountpoint = document.getElementById('dynamic-form')
+  if ( mountpoint ) {
+    React.render(<DynamicForm />, mountpoint);
+  };
 
 });
+
+
