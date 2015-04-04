cb5aba1413f4ac5f3f8cd272671e903418db79af
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-04-04 12:07:28 +0200

    PopulateAtCheckboxes is populated correctly.

--- app/assets/javascripts/react-components/dynamic-form.js.jsx
+++ app/assets/javascripts/react-components/dynamic-form.js.jsx
@@ -6,42 +6,94 @@ $( document ).ready( function()  {
     same_as           : null,
     autocomplete_from : null,
     title             : null
-  }
+  };
 
+  var data = {
+  populate_at: ['automatically', 'web_start', 'web_end'],
+  autocomplete_from: ['customer_name', 'customer_address',
+                        'project_address', 'worker_names']
+  };
+
+
+  var PopulateAtCheckboxes = React.createClass({
+    render: function() {
+      globProps= this.props;
+      var populateAtCheckbox = this.props.populate_at.map(function(value) {
+        return (
+          // `key` is a React-specific concept and is not mandatory for the
+          // purpose of this tutorial. if you're curious, see more here:
+          // http://facebook.github.io/react/docs/multiple-components.html#dynamic-children
+         <label for={value}>
+             <input type="checkbox" name={value} value="{value}"/>
+             {value}
+         </label>
+        );
+      });
+      return (
+        <div classname="populate-at-checkboxes">
+          {populateAtCheckbox}
+        </div>
+      );
+    }
+  });
+
+  var AutocompleteFromCheckboxes = React.createClass({
+    render: function() {
+      if (typeof autocomplete_from != "undefined") {
+         var option = autocomplete_from.map( function(value) {
+           return (
+             <label for={value}>
+               <input type="checkbox" name={value} value="{value}"/>
+               {value}
+             </label>
+           );
+         });
+         return (
+           <span> loading.. </span>
+         );
+      }
+      else {
+        return (
+          <span> NADA </span>
+        );
+      }
+    }
+  });
 
   var DynamicForm = React.createClass({
-    //getDefaultProps: function() {
-    //  pop_vals = [:automatically, :web_start, :web_end]
-    //  return { populate_at: pop_vals }
-    //}
+    saveAndContinue: function(e) {
+      e.preventDefault();
+      //var data = {
+      //  name     : this.refs.name.getDOMNode().value,
+      //};
+      //console.log('data: ' + data.name);
+    },
 
     render: function() {
+      globProps= this.props;
       return (
         <div>
-          <label>Name</label>
-          <input type="text" ref="name" defaultValue='hi' />
-          <Select data={this.props.populate_at} />
+          <label>Velg navn på felt</label>
+          <input type="text" ref="name" defaultValue='' />
+          <strong> Når skal verdien fylles inn? </strong>
+          <PopulateAtCheckboxes populate_at={this.props.data.populate_at} />
+
+          <strong> Autocomplete med data fra </strong>
+          <AutocompleteFromCheckboxes
+            autocomplete_from={this.props.data.autocomplete_from} />
+          <br/>
+          <br/>
           <button onClick={this.saveAndContinue}>Save and Continue</button>
         </div>
       );
-    },
-    saveAndContinue: function(e) {
-      e.preventDefault()
-
-      // Get values via this.refs
-      var data = {
-        name     : this.refs.name.getDOMNode().value,
-      }
-      console.log('data: ' + data['name'])
-
     }
-  })
+  });
 
 
-  var mountpoint = document.getElementById('dynamic-form')
+  var mountpoint = document.getElementById('dynamic-form');
   if ( mountpoint ) {
-    React.render(<DynamicForm />, mountpoint);
-  };
+    React.render(<DynamicForm data={data} />, mountpoint);
+  }
 
 });
 
