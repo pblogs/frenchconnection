d3f529497614bafd2f0b05b0a4f5a66973c6df06
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-04-20 13:59:18 +0200

    DynamicForm accepts_nested_attributes_for :rows

--- app/controllers/dynamic_forms_controller.rb
+++ app/controllers/dynamic_forms_controller.rb
@@ -1,9 +1,18 @@
 class DynamicFormsController < InheritedResources::Base
 
+  def create
+    Rails.logger.debug "in DynamicFormsController"
+    @form = DynamicForm.new(dynamic_form_params)
+    @form.save!
+  end
+
   private
 
-    def dynamic_form_params
-      params.require(:dynamic_form).permit(:field_name, :populate, :autocomplete_from, :title)
-    end
+
+  def dynamic_form_params
+    Rails.logger.debug "in dynamic_form_params"
+    params.require(:dynamic_form).permit(:rows)
+    #params.require(:rows).permit("rows")
+  end
 end
 
