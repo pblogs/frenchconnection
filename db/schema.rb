ac7cce304ff57f69a93af12a741b440e2835aed7
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-04-23 12:00:52 +0200

    change datatype of rows to json

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150421114358) do
+ActiveRecord::Schema.define(version: 20150423063235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -166,7 +166,7 @@ ActiveRecord::Schema.define(version: 20150421114358) do
 
   create_table "dynamic_forms", force: :cascade do |t|
     t.integer  "user_id"
-    t.text     "rows"
+    t.json     "rows"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "title"
