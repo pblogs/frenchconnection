9907d2212c7d146f926f94a6d6a5621bbdad91db
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-04-24 15:07:02 +0200

    Dynamicform submissions

--- app/controllers/dynamic_forms_controller.rb
+++ app/controllers/dynamic_forms_controller.rb
@@ -5,22 +5,17 @@ class DynamicFormsController < ApplicationController
 
   def show
     @dynamic_form = DynamicForm.find params[:id]
-    #@dynamic_form = DynamicForm.last
-
     respond_to do |format|
       format.html
-      #format.json { render json: {title: @dynamic_form.title} }
-      format.json { render json: {title: @dynamic_form.title, rows: @dynamic_form.rows}  }
+      format.json { render json: {id: @dynamic_form.id,
+                                  title: @dynamic_form.title,
+                                  rows: @dynamic_form.rows}  }
     end
   end
 
   def create
-    Rails.logger.debug  "rows: #{params[:rows]}"
-    Rails.logger.debug  "form_title: #{params[:rows][:form_title]}"
-    @form = @current_user
-              .dynamic_forms.new(title: params[:rows][:form_title],
-                                 rows:  params[:rows]
-                                )
+    @form = @current_user.dynamic_forms.new(title: params[:rows][:form_title],
+                                            rows:  params[:rows])
     @form.save!
     render json: dynamic_forms_path, status: :ok
   end
@@ -33,7 +28,11 @@ class DynamicFormsController < ApplicationController
     @form = @current_user.dynamic_forms.find(params[:id])
     @form.destroy
     redirect_to dynamic_forms_path(format: :html)
+  end
 
+  def submissions
+    @dynamic_form = DynamicForm.find  params[:dynamic_form_id]
+    @submissions  = Submission.where(dynamic_form_id: params[:dynamic_form_id]).all
   end
 
   private
