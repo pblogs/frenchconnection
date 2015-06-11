6ac9dd0cf394b8850baa427ca5342300663b39f8
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-06-11 12:11:10 +0200

    Remove dynamic_form test

--- db/schema.rb
+++ db/schema.rb
@@ -17,12 +17,12 @@ ActiveRecord::Schema.define(version: 20150611075318) do
   enable_extension "plpgsql"
 
   create_table "active_admin_comments", force: :cascade do |t|
-    t.string   "namespace",     limit: 255
+    t.string   "namespace"
     t.text     "body"
-    t.string   "resource_id",   limit: 255, null: false
-    t.string   "resource_type", limit: 255, null: false
+    t.string   "resource_id",   null: false
+    t.string   "resource_type", null: false
     t.integer  "author_id"
-    t.string   "author_type",   limit: 255
+    t.string   "author_type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -32,13 +32,13 @@ ActiveRecord::Schema.define(version: 20150611075318) do
   add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
 
   create_table "api_keys", force: :cascade do |t|
-    t.string  "name",         limit: 255
-    t.string  "access_token", limit: 255
+    t.string  "name"
+    t.string  "access_token"
     t.boolean "active"
   end
 
   create_table "artisans", force: :cascade do |t|
-    t.string   "name",       limit: 255
+    t.string   "name"
     t.integer  "tasks_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 20150611075318) do
   end
 
   create_table "attachments", force: :cascade do |t|
-    t.string   "document",    limit: 255
+    t.string   "document"
     t.integer  "project_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -60,9 +60,9 @@ ActiveRecord::Schema.define(version: 20150611075318) do
   end
 
   create_table "blog_articles", force: :cascade do |t|
-    t.string   "title",        limit: 255
+    t.string   "title"
     t.text     "content"
-    t.string   "locale",       limit: 255
+    t.string   "locale"
     t.boolean  "published"
     t.date     "publish_date"
     t.datetime "created_at"
@@ -72,19 +72,19 @@ ActiveRecord::Schema.define(version: 20150611075318) do
   end
 
   create_table "blog_images", force: :cascade do |t|
-    t.string   "image",       limit: 255
-    t.string   "description", limit: 255
+    t.string   "image"
+    t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "owner_type",  limit: 255
+    t.string   "owner_type"
     t.integer  "owner_id"
     t.boolean  "main"
   end
 
   create_table "blog_projects", force: :cascade do |t|
-    t.string   "title",        limit: 255
+    t.string   "title"
     t.text     "content"
-    t.string   "locale",       limit: 255
+    t.string   "locale"
     t.boolean  "published"
     t.date     "publish_date"
     t.datetime "created_at"
@@ -92,10 +92,10 @@ ActiveRecord::Schema.define(version: 20150611075318) do
   end
 
   create_table "blog_videos", force: :cascade do |t|
-    t.string   "title",        limit: 255
+    t.string   "title"
     t.text     "content"
-    t.string   "video_url",    limit: 255
-    t.string   "locale",       limit: 255
+    t.string   "video_url"
+    t.string   "locale"
     t.boolean  "published"
     t.date     "publish_date"
     t.datetime "created_at"
@@ -103,7 +103,7 @@ ActiveRecord::Schema.define(version: 20150611075318) do
   end
 
   create_table "certificates", force: :cascade do |t|
-    t.string   "title",      limit: 255
+    t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -127,39 +127,40 @@ ActiveRecord::Schema.define(version: 20150611075318) do
     t.integer  "runs_in_company_car"
     t.float    "km_driven_own_car"
     t.float    "toll_expenses_own_car"
-    t.string   "supplies_from_warehouse", limit: 255
+    t.string   "supplies_from_warehouse"
     t.integer  "piecework_hours"
     t.integer  "overtime_50"
     t.integer  "overtime_100"
     t.integer  "hour"
-    t.string   "text",                    limit: 255
+    t.string   "text"
     t.text     "reason"
     t.integer  "user_id"
   end
 
+  add_index "changes", ["hours_spent_id"], name: "index_changes_on_hours_spent_id", using: :btree
   add_index "changes", ["user_id"], name: "index_changes_on_user_id", using: :btree
 
   create_table "customer_messages", force: :cascade do |t|
-    t.string   "text",       limit: 255
+    t.string   "text"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "customers", force: :cascade do |t|
-    t.string   "name",           limit: 255
-    t.string   "address",        limit: 255
-    t.string   "org_number",     limit: 255
-    t.string   "contact_person", limit: 255
-    t.string   "phone",          limit: 255
+    t.string   "name"
+    t.string   "address"
+    t.string   "org_number"
+    t.string   "contact_person"
+    t.string   "phone"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "customer_nr"
-    t.string   "area",           limit: 255
-    t.string   "email",          limit: 255
+    t.string   "area"
+    t.string   "email"
   end
 
   create_table "departments", force: :cascade do |t|
-    t.string   "title",      limit: 255
+    t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -177,7 +178,7 @@ ActiveRecord::Schema.define(version: 20150611075318) do
   create_table "favorites", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "favorable_id"
-    t.string   "favorable_type", limit: 255
+    t.string   "favorable_type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -198,24 +199,24 @@ ActiveRecord::Schema.define(version: 20150611075318) do
     t.integer  "runs_in_company_car"
     t.float    "km_driven_own_car"
     t.float    "toll_expenses_own_car"
-    t.string   "supplies_from_warehouse", limit: 255
-    t.string   "of_kind",                 limit: 255, default: "personal"
+    t.string   "supplies_from_warehouse"
+    t.string   "of_kind",                 default: "personal"
     t.integer  "billable_id"
     t.integer  "personal_id"
-    t.boolean  "approved",                            default: false
+    t.boolean  "approved",                default: false
     t.text     "change_reason"
     t.text     "old_values"
-    t.boolean  "edited_by_admin",                     default: false
+    t.boolean  "edited_by_admin",         default: false
   end
 
   add_index "hours_spents", ["customer_id"], name: "index_hours_spents_on_customer_id", using: :btree
   add_index "hours_spents", ["task_id"], name: "index_hours_spents_on_task_id", using: :btree
 
   create_table "inventories", force: :cascade do |t|
-    t.string   "name",                             limit: 255
-    t.string   "description",                      limit: 255
+    t.string   "name"
+    t.string   "description"
     t.integer  "certificates_id"
-    t.boolean  "can_be_rented_by_other_companies",             default: false
+    t.boolean  "can_be_rented_by_other_companies", default: false
     t.integer  "rental_price_pr_day"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -229,7 +230,7 @@ ActiveRecord::Schema.define(version: 20150611075318) do
   end
 
   create_table "locations", force: :cascade do |t|
-    t.string   "name",            limit: 255
+    t.string   "name"
     t.integer  "certificates_id"
     t.boolean  "outdoor"
     t.boolean  "indoor"
@@ -240,8 +241,8 @@ ActiveRecord::Schema.define(version: 20150611075318) do
   create_table "mobile_pictures", force: :cascade do |t|
     t.integer  "task_id"
     t.integer  "user_id"
-    t.string   "url",         limit: 255
-    t.string   "description", limit: 255
+    t.string   "url"
+    t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "project_id"
@@ -252,24 +253,24 @@ ActiveRecord::Schema.define(version: 20150611075318) do
   add_index "mobile_pictures", ["user_id"], name: "index_mobile_pictures_on_user_id", using: :btree
 
   create_table "monthly_reports", force: :cascade do |t|
-    t.string   "document",   limit: 255
+    t.string   "document"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "month_nr"
-    t.string   "title",      limit: 255
+    t.string   "title"
     t.integer  "year"
   end
 
   create_table "professions", force: :cascade do |t|
-    t.string   "title",      limit: 255
+    t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "projects", force: :cascade do |t|
-    t.string   "project_number",                       limit: 255
-    t.string   "name",                                 limit: 255
+    t.string   "project_number"
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "customer_id"
@@ -277,17 +278,17 @@ ActiveRecord::Schema.define(version: 20150611075318) do
     t.date     "due_date"
     t.text     "description"
     t.integer  "user_id"
-    t.string   "execution_address",                    limit: 255
+    t.string   "execution_address"
     t.text     "customer_reference"
     t.text     "comment"
-    t.boolean  "sms_employee_if_hours_not_registered",             default: false
-    t.boolean  "sms_employee_when_new_task_created",               default: false
+    t.boolean  "sms_employee_if_hours_not_registered", default: false
+    t.boolean  "sms_employee_when_new_task_created",   default: false
     t.integer  "department_id"
-    t.boolean  "complete",                                         default: false
+    t.boolean  "complete",                             default: false
   end
 
   create_table "skills", force: :cascade do |t|
-    t.string   "title",      limit: 255
+    t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -314,7 +315,7 @@ ActiveRecord::Schema.define(version: 20150611075318) do
   add_index "submissions", ["user_id"], name: "index_submissions_on_user_id", using: :btree
 
   create_table "task_types", force: :cascade do |t|
-    t.string   "title",      limit: 255
+    t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -325,8 +326,8 @@ ActiveRecord::Schema.define(version: 20150611075318) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "accepted"
-    t.string   "description",      limit: 255
-    t.boolean  "finished",                     default: false
+    t.string   "description"
+    t.boolean  "finished",         default: false
     t.integer  "project_id"
     t.date     "due_date"
     t.datetime "ended_at"
@@ -334,8 +335,8 @@ ActiveRecord::Schema.define(version: 20150611075318) do
     t.integer  "location_id"
     t.integer  "profession_id"
     t.integer  "skills_ids"
-    t.boolean  "draft",                        default: true
-    t.string   "address",          limit: 255
+    t.boolean  "draft",            default: true
+    t.string   "address"
   end
 
   add_index "tasks", ["customer_id"], name: "index_tasks_on_customer_id", using: :btree
@@ -348,38 +349,38 @@ ActiveRecord::Schema.define(version: 20150611075318) do
   end
 
   create_table "user_tasks", force: :cascade do |t|
-    t.integer  "user_id",                null: false
-    t.integer  "task_id",                null: false
-    t.string   "status",     limit: 255, null: false
+    t.integer  "user_id",    null: false
+    t.integer  "task_id",    null: false
+    t.string   "status",     null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255
-    t.string   "encrypted_password",     limit: 255,             null: false
-    t.string   "roles",                                                       array: true
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email"
+    t.string   "encrypted_password",                           null: false
+    t.string   "roles",                                                     array: true
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0, null: false
+    t.integer  "sign_in_count",                    default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "first_name",             limit: 255
-    t.string   "last_name",              limit: 255
+    t.string   "first_name"
+    t.string   "last_name"
     t.integer  "department_id"
     t.integer  "mobile",                 limit: 8
-    t.string   "employee_nr",            limit: 255
-    t.string   "image",                  limit: 255
-    t.string   "emp_id",                 limit: 255
+    t.string   "employee_nr"
+    t.string   "image"
+    t.string   "emp_id"
     t.integer  "profession_id"
-    t.string   "home_address",           limit: 255
-    t.string   "home_area_code",         limit: 255
-    t.string   "home_area",              limit: 255
+    t.string   "home_address"
+    t.string   "home_area_code"
+    t.string   "home_area"
     t.integer  "roles_mask"
   end
 
@@ -387,11 +388,11 @@ ActiveRecord::Schema.define(version: 20150611075318) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "zipped_reports", force: :cascade do |t|
-    t.integer  "project_id",              null: false
-    t.string   "zipfile",     limit: 255
+    t.integer  "project_id",  null: false
+    t.string   "zipfile"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "report_type", limit: 255
+    t.string   "report_type"
   end
 
   add_foreign_key "dynamic_forms", "users"
