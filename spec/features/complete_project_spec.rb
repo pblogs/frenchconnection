9f6f0b1990c3aecfc09d8111f200b6b329f6ae0a
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-06-16 18:33:31 +0200

    fix test

--- spec/features/complete_project_spec.rb
+++ spec/features/complete_project_spec.rb
@@ -17,7 +17,7 @@ feature 'Project leader completes a project', type: :feature do
 
     visit customer_project_path(@project.customer, @project)
     Sms.should_receive(:send_msg).with(hash_including(to: "47#{@worker.mobile}"))
-    expect { page.first('.action_buttons a.complete').click }
+    expect { click_link( I18n.t('project.complete_link') ) }
       .to change(Project.where(complete: true), :count).by(1)
 
   end
