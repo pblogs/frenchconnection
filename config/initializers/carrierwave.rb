394ca5b3ba126afc05582ce2a2933df1525738fb
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-07-06 19:47:28 +0200

    Use ENV['SHORT_NAME’] as fog_directory

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ if Rails.env.production?
       :aws_secret_access_key  => ENV['STABENFELDT_SECRET_ACCESS_KEY'],
       :region                 => 'eu-west-1',
     }
-    config.fog_directory  = ENV['FOG_DIRECTORY']                       # required
+    config.fog_directory  = ENV['SHORT_NAME']                       # required
     config.fog_public     = false                                   # optional, defaults to true
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
   end
