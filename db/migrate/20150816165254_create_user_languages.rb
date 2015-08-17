32da9b0298464ed675000b685f49eba74c66e26e
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-08-17 21:36:15 +0200

    working with controller specs

--- db/migrate/20150816165254_create_user_languages.rb
+++ db/migrate/20150816165254_create_user_languages.rb
@@ -1,9 +1,9 @@
 class CreateUserLanguages < ActiveRecord::Migration
   def change
     create_table :user_languages do |t|
-      t.belongs_to :user, null: false
-      t.belongs_to :language, null: false
-      t.integer :rating, null: false
+      t.belongs_to :user
+      t.belongs_to :language
+      t.integer :rating
       t.timestamps null: false
     end
   end
--- spec/fabricators/user_language_fabricator.rb
+++ spec/fabricators/user_language_fabricator.rb
@@ -1,3 +1,5 @@
 Fabricator(:user_language) do
   rating 1
+  user { Fabricate(:user) }
+  language { Fabricate(:language) }
 end
