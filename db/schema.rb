e1b1a238e63afb9a046ef713221c30a80879dea2
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-08-27 21:01:41 +0200

    fix bug

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150823130250) do
+ActiveRecord::Schema.define(version: 20150825153104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -401,6 +401,7 @@ ActiveRecord::Schema.define(version: 20150823130250) do
     t.string   "address"
     t.date     "birth_date"
     t.text     "relatives"
+    t.string   "initials"
   end
 
   add_index "users", ["profession_id"], name: "index_users_on_profession_id", using: :btree
