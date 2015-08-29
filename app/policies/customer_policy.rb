a2fb33f1c8a74ebf82a51ca2b590ed9e297f5903
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-08-29 14:45:20 +0200

    customer policy

---
+++ app/policies/customer_policy.rb
@@ -0,0 +1,58 @@
+class CustomerPolicy  < ApplicationPolicy
+  attr_reader :user, :project
+
+  def initialize(user, project)
+    @user = user
+    @project = project
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def update?
+    admin_or_project_leader?
+  end
+
+  def edit?
+    admin_or_project_leader?
+  end
+
+  def destroy?
+    admin?
+  end
+
+  def create?
+    admin_or_project_leader?
+  end
+
+  def complete?
+    admin_or_project_leader?
+  end
+
+  def billable_hours?
+    true
+  end
+
+  def personal_hours?
+    true
+  end
+
+  def daily_report?
+    true
+  end
+
+  private
+  def admin_or_project_leader?
+    user.is?(:project_leader) || user.is?(:admin)
+  end
+
+  def admin?
+    user.is?(:admin)
+  end
+
+end
