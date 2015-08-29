f85fc5115bf37d08481a6039960c59f6f6c0a684
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-08-29 10:38:54 +0200

    fix bug

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -44,6 +44,7 @@ class ProjectsController < ApplicationController
   # POST /projects.json
   def create
     @project = Project.new(project_params)
+    @project.user = @current_user
     authorize @project
 
     respond_to do |format|
