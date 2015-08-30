bd2090d56d96b617810af0ffe4908a93523f7041
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-08-30 12:22:00 +0200

    whitespace

--- spec/api/inventories_spec.rb
+++ spec/api/inventories_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 
 describe V1::Inventories do
-  
+
   describe 'GET /api/v1/inventories/inventories' do
     it 'lists all inventories' do
-      inventory1 = Fabricate(:inventory) 
+      inventory1 = Fabricate(:inventory)
       inventory2 = Fabricate(:inventory)
       inventory3 = Fabricate(:inventory)
       get "/api/v1/inventories/"
       response.status.should == 200
       parsed_body = JSON.parse(response.body)
       ids = parsed_body.collect { |u| u['id'] }
-      ids.should =~ [inventory3.id, inventory2.id, inventory1.id] 
+      ids.should =~ [inventory3.id, inventory2.id, inventory1.id]
     end
   end
 
@@ -25,5 +25,5 @@ describe V1::Inventories do
       u['name'].should eq inventory.name
     end
   end
-  
+
 end
