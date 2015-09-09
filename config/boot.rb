4eca44d24ba24fbfcc9e2f4c4c400289320f8596
Pascal <pknoth.cbm@gmail.com>
2015-09-09 16:04:36 +0200

    fog fake account

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,8 @@
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
+ENV['STABENFELDT_AWS_ACCESS_KEY_ID'] ||= 'test'
+ENV['STABENFELDT_SECRET_ACCESS_KEY'] ||= 'test'
+ENV['SHORT_NAME'] ||= 'test'
+
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
