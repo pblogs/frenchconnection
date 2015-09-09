66c1a80c4bf77a25b5776525a5d99a89afc3acdd
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-09-09 20:28:31 +0200

    Disable ActiveRecord and ActiveRecord fixtures.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,9 +7,6 @@ require 'pundit/rspec'
 ActiveRecord::Migration.maintain_test_schema!
 
 
-
-
-
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
@@ -40,7 +37,7 @@ RSpec.configure do |config|
 
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
 
   # If true, the base class of anonymous controllers will be inferred
