c009bca10a91093996ca2678127f6b6924877acd
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-09-09 20:23:23 +0200

    Added missing before_action :get_settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_current_user
   before_action :authenticate_user!
+  before_action :get_settings
 
 
   respond_to :html, :json
