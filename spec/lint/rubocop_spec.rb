c3649012e33701842bc964012e3a0880b21b1197
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-09-12 22:55:50 +0200

    Test that changed files are according to standard.

---
+++ spec/lint/rubocop_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe 'Check that the files we have changed have correct syntax' do
+  before do
+    current_sha = `git rev-parse --verify HEAD`.strip!
+    files = `git diff master #{current_sha} --name-only | egrep -v "(slim|yml)"`
+    files.gsub!("\n", ' ')
+    puts "Change files: #{files}"
+
+    @report = `rubocop #{files}`
+    puts "Report: #{@report}"
+  end
+
+  it { @report.match('Offenses').should be false }
+end
