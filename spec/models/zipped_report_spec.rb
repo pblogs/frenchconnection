9ad77267f624663eb42508bfa825769eda35db33
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-09-12 15:59:10 +0200

    fix rubocop warnings

--- spec/models/zipped_report_spec.rb
+++ spec/models/zipped_report_spec.rb
@@ -13,19 +13,17 @@
 require 'spec_helper'
 
 describe ZippedReport do
-  describe "cleanup_old_reports" do
-
+  describe 'cleanup_old_reports' do
     before do
       @report       = Fabricate :zipped_report
       @other_report = Fabricate :zipped_report
     end
 
-    it "cleans up properly" do
+    it 'cleans up properly' do
       fresh_report = ZippedReport.create(
-          project: @report.project, report_type: @report.report_type)
-      expect { fresh_report.cleanup_old_reports 
-      }.to change(ZippedReport, :count).by(-1)
+              project: @report.project, report_type: @report.report_type)
+      expect { fresh_report.cleanup_old_reports }
+        .to change(ZippedReport, :count).by(-1)
     end
-
   end
 end
