5fa6191efa1c01dd190029cda7a28cd0ae833b64
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-09-13 09:45:15 +0200

    enhance rubocop check

--- spec/lint/rubocop_spec.rb
+++ spec/lint/rubocop_spec.rb
@@ -5,10 +5,13 @@ describe 'Check that the files we have changed have correct syntax' do
     current_sha = `git rev-parse --verify HEAD`.strip!
     files = `git diff master #{current_sha} --name-only | grep .rb`
     files.tr!("\n", ' ')
-    puts "Change files: #{files}"
+    @report = 'nada'
+    if files.present?
+      puts "Changed files: #{files}"
 
-    @report = `rubocop #{files}`
-    puts "Report: #{@report}"
+      @report = `rubocop #{files}`
+      puts "Report: #{@report}"
+    end
   end
 
   it { @report.match('Offenses').should_not be true }
