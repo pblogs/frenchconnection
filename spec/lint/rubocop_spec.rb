63eb09bad89eef1f1e8b9d6182403f58393a9d9f
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-09-14 12:22:56 +0200

    Get changed files from api.gitub.com

--- spec/lint/rubocop_spec.rb
+++ spec/lint/rubocop_spec.rb
@@ -3,7 +3,10 @@ require 'spec_helper'
 describe 'Check that the files we have changed have correct syntax' do
   before do
     current_sha = `git rev-parse --verify HEAD`.strip!
-    files = `git diff master #{current_sha} --name-only | grep .rb`
+    token = '316129065b398bed3730ce243b6f0f508354221e'
+    url = 'https://api.github.com/repos/orwapp/orwapp/compare/' \
+          "master...#{current_sha}?access_token=#{token}"
+    files = `curl -i #{url} | grep filename | cut -f2 -d: | grep \.rb | tr '"', '\ '`
     files.tr!("\n", ' ')
     @report = 'nada'
     if files.present?
@@ -14,5 +17,5 @@ describe 'Check that the files we have changed have correct syntax' do
     end
   end
 
-  it { @report.match('Offenses').should_not be true }
+  it { expect(@report.match('Offenses')).to be_falsey }
 end
