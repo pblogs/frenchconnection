0fd0de0b9ebf5163f27599b282bb61419954c09b
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-09-15 16:08:59 +0200

    Output report

--- spec/lint/rubocop_spec.rb
+++ spec/lint/rubocop_spec.rb
@@ -1,19 +1,11 @@
 require 'spec_helper'
 
-describe 'Check that the files we have changed have correct syntax' do
+describe 'Rubocop checks that the files we have changed have correct syntax' do
   before do
     current_sha = `git rev-parse --verify HEAD`.strip!
-    token = '316129065b398bed3730ce243b6f0f508354221e'
-    url = 'https://api.github.com/repos/orwapp/orwapp/compare/' \
-          "master...#{current_sha}?access_token=#{token}"
-    files = `curl -i #{url} | grep filename | cut -f2 -d: | grep \.rb | tr '"', '\ '`
+    files = `git diff master #{current_sha} --name-only | grep .rb`
     files.tr!("\n", ' ')
     files.gsub!('db/schema.rb', '')
-<<<<<<< HEAD
-    @report = 'nada'
-    if files.present?
-      puts "Changed files: #{files}"
-=======
     files.gsub!('Gemfile', '')
     files.gsub!('Gemfile.lock', '')
     cleaned = remove_missing_files(files)
@@ -23,12 +15,13 @@ describe 'Check that the files we have changed have correct syntax' do
   end
 
   it { expect(@report.match('Offenses')).to be_falsey }
->>>>>>> 3244ef5... Output report
 
-      @report = `rubocop #{files}`
-      puts "Report: #{@report}"
+  def remove_missing_files(files)
+    cleaned = []
+    files.split.each do |file|
+      next              unless File.exist? file
+      cleaned << file
     end
+    cleaned.join(' ')
   end
-
-  it { expect(@report.match('Offenses')).to be_falsey }
 end
