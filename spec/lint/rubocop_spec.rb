3810d5a99afc28522633ede4943b51b2c55abf5b
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-09-15 16:24:19 +0200

    Check local files if we’re not on Circle.

--- spec/lint/rubocop_spec.rb
+++ spec/lint/rubocop_spec.rb
@@ -6,11 +6,17 @@ describe 'Check that the files we have changed have correct syntax' do
     token = '316129065b398bed3730ce243b6f0f508354221e'
     url = 'https://api.github.com/repos/orwapp/orwapp/compare/' \
           "master...#{current_sha}?access_token=#{token}"
-    files = `curl -i #{url} | grep filename | cut -f2 -d: | grep \.rb | tr '"', '\ '`
+    puts "URL is #{url}"
+    if ENV['CI']
+      files = `curl -i #{url} | grep filename | cut -f2 -d: | grep \.rb | tr '"', '\ '`
+    else
+      files = `git diff master #{current_sha} --name-only | grep .rb`
+    end
     files.tr!("\n", ' ')
     files.gsub!('Gemfile', '')
     files.gsub!('Gemfile.lock', '')
     cleaned = remove_missing_files(files)
+    puts "FILES: #{files}"
     @report = 'nada'
     if cleaned.present?
       puts "Changed files: #{cleaned}"
