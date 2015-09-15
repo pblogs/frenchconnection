4a7fb2275b4cfb0cc454995563100e6730b2c76d
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-09-15 16:17:49 +0200

    print report

--- spec/lint/rubocop_spec.rb
+++ spec/lint/rubocop_spec.rb
@@ -1,17 +1,23 @@
 require 'spec_helper'
 
-describe 'Rubocop checks that the files we have changed have correct syntax' do
+describe 'Check that the files we have changed have correct syntax' do
   before do
     current_sha = `git rev-parse --verify HEAD`.strip!
-    files = `git diff master #{current_sha} --name-only | grep .rb`
+    token = '316129065b398bed3730ce243b6f0f508354221e'
+    url = 'https://api.github.com/repos/orwapp/orwapp/compare/' \
+          "master...#{current_sha}?access_token=#{token}"
+    files = `curl -i #{url} | grep filename | cut -f2 -d: | grep \.rb | tr '"', '\ '`
     files.tr!("\n", ' ')
-    files.gsub!('db/schema.rb', '')
     files.gsub!('Gemfile', '')
     files.gsub!('Gemfile.lock', '')
     cleaned = remove_missing_files(files)
-    @report = ''
-    @report = `rubocop #{cleaned}` if cleaned.present?
-    puts "Rubocop report: #{@report}"
+    @report = 'nada'
+    if cleaned.present?
+      puts "Changed files: #{cleaned}"
+
+      @report = `rubocop #{cleaned}`
+      puts "Report: #{@report}"
+    end
   end
 
   it { expect(@report.match('Offenses')).to be_falsey }
