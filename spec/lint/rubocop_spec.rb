c0c46048a692dfeb5c97094a6394873cf77ebe6b
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-09-15 16:08:21 +0200

    Output report

--- spec/lint/rubocop_spec.rb
+++ spec/lint/rubocop_spec.rb
@@ -9,9 +9,21 @@ describe 'Check that the files we have changed have correct syntax' do
     files = `curl -i #{url} | grep filename | cut -f2 -d: | grep \.rb | tr '"', '\ '`
     files.tr!("\n", ' ')
     files.gsub!('db/schema.rb', '')
+<<<<<<< HEAD
     @report = 'nada'
     if files.present?
       puts "Changed files: #{files}"
+=======
+    files.gsub!('Gemfile', '')
+    files.gsub!('Gemfile.lock', '')
+    cleaned = remove_missing_files(files)
+    @report = ''
+    @report = `rubocop #{cleaned}` if cleaned.present?
+    puts "Rubocop report: #{@report}"
+  end
+
+  it { expect(@report.match('Offenses')).to be_falsey }
+>>>>>>> 3244ef5... Output report
 
       @report = `rubocop #{files}`
       puts "Report: #{@report}"
